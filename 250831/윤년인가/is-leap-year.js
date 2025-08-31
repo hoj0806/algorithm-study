5f8078942a4d8ecc81d0137fe69383e3654f55cc
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 12:38:59 +0900

    Title: 윤년인가, Time: 172ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250831/윤년인가/is-leap-year.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim()
+
+let year = Number(input)
+
+
+if(year % 4 === 0) {
+    if(year % 100 === 0 && year % 400 !== 0) {
+        console.log(false)
+    } else {
+        console.log(false)
+    } 
+        console.log(true)
+    
+} else {
+    console.log(false)
+}
