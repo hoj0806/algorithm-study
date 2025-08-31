2fc9df96055593999a257ae2477f0b2122c06af9
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 13:37:44 +0900

    Title: a로 채워넣기, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250831/a로 채워넣기/filling-with-a.js
+++ 250831/a로 채워넣기/filling-with-a.js
@@ -3,4 +3,4 @@ let input = fs.readFileSync(0).toString().trim()
 
 let str = input
 
-console.log(str.slice(0, 1) + "a" + str.slice(2, - 2))
+console.log(str.slice(0, 1) + "a" + str.slice(2, - 2) + "a" + str.slice(-1))
