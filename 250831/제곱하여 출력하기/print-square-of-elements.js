ecc9523c97a010c956c6fc360527a28672a02fa3
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 13:06:12 +0900

    Title: 제곱하여 출력하기, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250831/제곱하여 출력하기/print-square-of-elements.js
+++ 250831/제곱하여 출력하기/print-square-of-elements.js
@@ -6,7 +6,9 @@ let a = Number(input[0])
 let arr = input[1].split(" ").map((n) => Number(n))
 
 let answer = ''
-for(let i = 0; i <= a; i++) {
-    answer += `${i**2} `
+
+for(let i = 0; i < a; i++) {
+    answer += `${arr[i]**2} `
 }
+
 console.log(answer)
