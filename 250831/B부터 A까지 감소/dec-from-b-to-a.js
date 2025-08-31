7381f267b5c3caa155a85f28d81d3927f23ec464
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 11:56:54 +0900

    Title: B부터 A까지 감소, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250831/B부터 A까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split(" ")
+
+let a = Number(input[0])
+let b = Number(input[1])
+
+
+let answer = ''
+for(let i = b; i >= a; i--) {
+    answer += `${i} `
+}
