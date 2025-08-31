7fbed4fda0088e189f7526b3ee3ecd8bc758fa32
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 11:41:34 +0900

    Title: N번 더하기, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250831/N번 더하기/add-n-times.js
+++ 250831/N번 더하기/add-n-times.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split(" ")
 let a = Number(input[0])
 let b = Number(input[1])
 
-for(let i = 0; i <= b; i++) {
+for(let i = 0; i < b; i++) {
     a+=b
     console.log(a)
 }
