cd52e1a6c5acb4651289156c92abc63cebe330e8
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 14:05:42 +0900

    Title: 친근하지 않은 수, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

--- 250831/친근하지 않은 수/unfriendly-number.js
+++ 250831/친근하지 않은 수/unfriendly-number.js
@@ -6,11 +6,10 @@ let n = Number(input)
 let answer = 0
 
 for(let i = 1; i <= n; i++) {
-    if((i % 2 === 0 && i % 3 === 0) || i % 5 === 0) {
-        console.log(i)
-    } else {
+    if((i % 2 === 0 || i % 3 === 0) || i % 5 === 0) {
         continue
-    }
+    } 
+    answer+=1
 }
 
 console.log(answer)
