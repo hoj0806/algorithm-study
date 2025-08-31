78149b6146d85f1516dfe27b2c42223233298870
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-08-31 13:15:22 +0900

    Title: 일의 자리 배열, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250831/일의 자리 배열/array-with-units.js
+++ 250831/일의 자리 배열/array-with-units.js
@@ -3,11 +3,12 @@ let input = fs.readFileSync(0).toString().trim().split(" ")
 
 let arr = []
 
-for(let i = 0; i < 9; i++) {
+for(let i = 0; i <= 9; i++) {
     if(i === 0 || i === 1) {
         arr.push(Number(input[i]))
     } else {
-        console.log(arr[i-1] + arr[i-2])
+        let sum = arr[i-1] + arr[i-2]
+        arr.push(sum % 10)
     }
 }
 
