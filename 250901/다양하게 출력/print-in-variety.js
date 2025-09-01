ebdbec9a9984ccdc7035ddc9c03fbb818fa8b4ec
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-01 17:52:39 +0900

    Title: 다양하게 출력, Time: 60ms, Memory: 10MB, Status: Passed - Codetree

--- 250901/다양하게 출력/print-in-variety.js
+++ 250901/다양하게 출력/print-in-variety.js
@@ -1,4 +1,4 @@
-console.log(`Total days in Year`)
-console.log(365)
-console.log(`Circumference rate`)
-console.log(3.1415926535)
+console.log(`Total days in Year
+365
+Circumference rate
+3.1415926535`);
