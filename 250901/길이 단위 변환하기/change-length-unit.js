e1dfd42773fe073d40e14ce0aa9c3097237e4b1e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-01 19:14:51 +0900

    Title: 길이 단위 변환하기, Time: 55ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250901/길이 단위 변환하기/change-length-unit.js
+++ 250901/길이 단위 변환하기/change-length-unit.js
@@ -1,3 +1,3 @@
 let [a, b] = [9.2, 1.3]
-consolelog(`${a}ft = ${(a * 30.48).toFixed(1)}`)
-consolelog(`${b}mi = ${(b * 1.3).toFixed(1)}`)
+console.log(`${a}ft = ${(a * 30.48).toFixed(1)}`)
+console.log(`${b}mi = ${(b * 1.3).toFixed(1)}`)
