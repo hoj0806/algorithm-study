a3563290dcac5e85ca05d36e272e6b0488516c66
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-01 23:45:12 +0900

    Title: 실수와 문자 받아 출력하기, Time: 60ms, Memory: 10MB, Status: Passed - Codetree

--- 250901/실수와 문자 받아 출력하기/print-number--and-letter.js
+++ 250901/실수와 문자 받아 출력하기/print-number--and-letter.js
@@ -4,5 +4,5 @@ let input = fs.readFileSync(0).toString().trim().split('\n')
 let [str, a, b] = input
 
 console.log(str)
-console.log(a.toFixed(2))
-console.log(b.toFixed(2))
+console.log(Number(a).toFixed(2))
+console.log(Number(b).toFixed(2))
