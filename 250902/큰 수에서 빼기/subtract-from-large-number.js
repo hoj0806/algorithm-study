c24d71221c35346865ec02eeb43118fd8eb02b8b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 12:28:00 +0900

    Title: 큰 수에서 빼기, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250902/큰 수에서 빼기/subtract-from-large-number.js
+++ 250902/큰 수에서 빼기/subtract-from-large-number.js
@@ -3,10 +3,13 @@ let input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 
 let [a, b] = input
 
+if(a !== b) {
+
+
 if(a > b) {
     console.log(a - b)
 }
 
 if(b > a) {
     console.log(b - a)
-}
+}}
