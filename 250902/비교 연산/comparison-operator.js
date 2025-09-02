e5770ac6a9356d72e2ef892d8e0224c4e39b9827
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:27:31 +0900

    Title: 비교 연산, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/비교 연산/comparison-operator.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let [a, b] = input
+
+console.log(a >= b ? 1 : 0)
+console.log(a > b ? 1 : 0)
+console.log(a <= b ? 1 : 0)
+console.log(a < b ? 1 : 0)
+console.log(a === b ? 1 : 0)
+console.log(a !== b ? 1 : 0)
