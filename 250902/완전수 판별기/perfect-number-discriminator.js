8e775c963909e46374694610b5e33ca399f6748b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 23:35:56 +0900

    Title: 완전수 판별기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/완전수 판별기/perfect-number-discriminator.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+let sum = 0
+
+for(let i = 1; i < n; i++) {
+    if(n % i === 0) {
+        sum+=i
+    }
+}
+
+console.log(sum === n ? 'P' : 'N')
