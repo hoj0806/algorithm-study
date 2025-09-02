5d5ab387df831350d56003848bdf094dd9e90a20
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:35:11 +0900

    Title: 점수 비교, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250902/점수 비교/score-comparison.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split("\n")
+
+let[mathA, englishA] = input[0].split(" ").map(Number)
+let[mathB, englishB] = input[1].split(" ").map(Number)
+
+if(mathA > mathB && englishA && englishB) {
+    console.log(1)
+} else {
+    console.log(0)
+}
+
+
+
