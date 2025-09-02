537ae5c0c95e71e29f07b49c976fc0a559ef9183
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 14:12:27 +0900

    Title: 두 사람, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/두 사람/two-person.js
+++ 250902/두 사람/two-person.js
@@ -5,7 +5,7 @@ let [ageA, genderA] = input[0].split(" ")
 let [ageB, genderB] = input[1].split(" ")
 
     
-if((Number(ageA) >= 19 && genderA === 'M') || Number(ageB) >= 19 && genderB === 'M') {
+if((Number(ageA) >= 19 && genderA === 'M') || (Number(ageB) >= 19 && genderB === 'M')) {
     console.log(1)
 } else {
     console.log(0)
