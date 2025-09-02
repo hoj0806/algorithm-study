227311f6e76f4324637d4d5a3a7ef53c6af5a138
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:25:39 +0900

    Title: 3 또는 5의 배수, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/3 또는 5의 배수/multiples-of-3-or-5.js
+++ 250902/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -4,13 +4,13 @@ const input = fs.readFileSync(0).toString().trim()
 let a = Number(input)
 
 if(a % 3 === 0) {
-    console.log('Yes')
+    console.log('YES')
 } else {
-    console.log('No')
+    console.log('NO')
 }
 
 if(a % 5 === 0) {
-    console.log('Yes')
+    console.log('YES')
 } else {
-    console.log('No')
+    console.log('NO')
 }
