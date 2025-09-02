fc2114f89cecbc75263f82a55ae2dffb02c1ad62
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 15:57:50 +0900

    Title: A/B 출력, Time: 78ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250902/A/B 출력/a-divide-b.js
+++ 250902/A/B 출력/a-divide-b.js
@@ -3,4 +3,9 @@ const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 
 let [a, b] = input
 
-console.log((a / b).toFixed(40))
+console.log((a / b))
+
+let answer = `${parseInt(a / b)}.`
+
+console.log(answer)
+
