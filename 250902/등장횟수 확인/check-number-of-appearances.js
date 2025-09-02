e91c236116bc0456802667f0d25110184c76982d
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 23:14:07 +0900

    Title: 등장횟수 확인, Time: 67ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250902/등장횟수 확인/check-number-of-appearances.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+
+let cnt = 0
+
+for(let i = 0; i < input.length; i++) {
+    let n = inpput[i]
+    if(i % 2 === 0) {
+        cnt++
+    }
+}
+
+console.log(cnt)
