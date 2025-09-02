f99fc0aa51314a79b026bac010e7647e7c0d5029
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 23:14:20 +0900

    Title: 등장횟수 확인, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250902/등장횟수 확인/check-number-of-appearances.js
+++ 250902/등장횟수 확인/check-number-of-appearances.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
 let cnt = 0
 
 for(let i = 0; i < input.length; i++) {
-    let n = inpput[i]
+    let n = input[i]
     if(i % 2 === 0) {
         cnt++
     }
