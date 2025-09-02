e45a6e2b74e607c7bb0b7d7c61c537198ae00ca7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 22:56:28 +0900

    Title: 369 게임, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250902/369 게임/369-game.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+let answer = ''
+for(let i = 1; i <= n; i++) {
+    if((i % 3 === 0) || 
+    (String(i).indexOf("3") !== -1 || 
+    String(i).indexOf("6") !== -1 || 
+    String(i).indexOf("9") !== -1 
+    )) {
+        answer += '0 '
+    } else {
+        answer += i + " "
+    }
+}
