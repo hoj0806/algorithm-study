7075fb91211c1d357f8ed025acf90705f9f365ff
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:29:01 +0900

    Title: 4가지 관계연산자, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/4가지 관계연산자/four-relational-operators.js
+++ 250902/4가지 관계연산자/four-relational-operators.js
@@ -4,6 +4,6 @@ const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 let [a, b] = input
 
 console.log(a >= b ? 1 : 0)
-console.log(a < b ? 1 : 0)
+console.log(a > b ? 1 : 0)
 console.log(a <= b ? 1 : 0)
 console.log(a < b ? 1 : 0)
