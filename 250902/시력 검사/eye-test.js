566ccea2c573733a8111973e3aa2b3972b9ac1c7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:39:21 +0900

    Title: 시력 검사, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/시력 검사/eye-test.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+
+let [a, b] = input
+
+if(a >= 1.0 && b >= 1.0) {
+    console.log("High")
+} else if(a >= 0.5 && b >= 0.5) {
+    console.log('Middle')   
+} else {
+    console.log('Low')
+}
