e7b4759db3268b33c889da558d6e034e0d177dfa
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 12:54:38 +0900

    Title: 출석 부르기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/출석 부르기/calling-attendance.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+if(n === 1) {
+    console.log('John')
+} else if(n === 2) {
+    console.log('Tom')
+} else if(n === 3) {
+    console.log('Paul')
+} else {
+    console.log('Vacancy')
+}
