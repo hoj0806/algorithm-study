d6bb7365bdc55d9152c23d5a3197481a7c4df336
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 15:03:16 +0900

    Title: 코로나 메뉴얼, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/코로나 메뉴얼/covid-manual.js
+++ 250902/코로나 메뉴얼/covid-manual.js
@@ -7,20 +7,17 @@ let [pC, tC] = input[2].split(" ")
 
 
 let count = 0
+
 if(pA === 'Y' && Number(tA) >= 37) {
-    count++
-}
-
-if(pB === 'Y' && Number(tB) >= 37) {
-    count++
-}
-
-if(pC === 'Y' && Number(tC) >= 37) {
-    count++
-}
-
-if(count >= 2) {
-    console.log('E')
+    if(pB === 'Y' && Number(tB) >= 37 || pC === 'Y' && Number(tC) >= 37) {
+        console.log("E")
+    } else {
+        console.log('N')
+    }
 } else {
-    console.log('N')
+     if(pB === 'Y' && Number(tB) >= 37 && pC === 'Y' && Number(tC) >= 37) {
+        console.log("E")
+    } else {
+        console.log('N')
+    }
 }
