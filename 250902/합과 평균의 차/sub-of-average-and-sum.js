3855ad8f40c87da505e8afc0eaff978b5172bf4c
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 12:22:35 +0900

    Title: 합과 평균의 차, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/합과 평균의 차/sub-of-average-and-sum.js
+++ 250902/합과 평균의 차/sub-of-average-and-sum.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
-let input = fs.readFileSync(0).toString().trim().split('\n')
+let input = fs.readFileSync(0).toString().trim().split(' ')
 
 let [a, b, c] = input.map(Number)
 
 let add = a + b + c
 let average = add / 3
 
-console.log(a, b, c)
+console.log(add)
 console.log(average)
 console.log(add - average)
