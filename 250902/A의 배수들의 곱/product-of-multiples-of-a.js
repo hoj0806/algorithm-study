ad344c5d623aec95f17de8b53a310afa325fc8d6
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 23:45:47 +0900

    Title: A의 배수들의 곱, Time: 63ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250902/A의 배수들의 곱/product-of-multiples-of-a.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let p = 1
+
+for(let i = 1; i <= b; i++) {
+    if(i % a === 0) {
+        p+=i
+    }
+}
+
+console.log(p)
