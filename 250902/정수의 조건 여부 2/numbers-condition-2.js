1b0d76d7799bcfae41ff13ee9f6e85bf1289468e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:03:56 +0900

    Title: 정수의 조건 여부 2, Time: 61ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/정수의 조건 여부 2/numbers-condition-2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let a = Number(input)
+
+if(a === 5) {
+    console.log('A')
+}
+
+if(a % 2 === 0) {
+    console.log("B")
+}
