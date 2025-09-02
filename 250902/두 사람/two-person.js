4ee60466501c0f2896fd692c5c37d34460e257f4
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 14:09:23 +0900

    Title: 두 사람, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250902/두 사람/two-person.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n')
+
+let [ageA, genderA] = input[0]
+let [ageB, genderB] = input[1]
+
+if((Number(ageA) >= 19 && genderA === 'M') || Number(ageB) >= 19 && genderB === 'M') {
+    console.log(1)
+} else {
+    console.log(0)
+}
