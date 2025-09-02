aa1dc5308c71e12372f20edfc316514d9f07756a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 15:30:36 +0900

    Title: 홀수만 출력, Time: 60ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/홀수만 출력/output-only-odd.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let [a, b] = input
+
+let answer = ''
+
+for(let i = a; i <= b; i++) {
+    if(i % 2 !== 0) {
+        answer += i + " "
+    }
+}
+
+console.log(answer)
