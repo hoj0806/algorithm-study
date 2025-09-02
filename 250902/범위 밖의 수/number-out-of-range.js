9e85897a4f4016fe4bc3dc33ba43fa2713c37790
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:48:45 +0900

    Title: 범위 밖의 수, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/범위 밖의 수/number-out-of-range.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let a = Number(input)
+
+if(a < 10 || a > 20) {
+    console.log('yes')
+} else {
+    console.log('no')
+}
