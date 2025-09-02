1c2d668af7d74ce479f6306db9297da6f178b6c3
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 14:44:52 +0900

    Title: 일 수 구하기, Time: 64ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250902/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let month = Number(inpt)
+
+if(month === 2) {
+    console.log(28)
+} else {
+    if(month === 4 || month === 6 || month === 9 || month === 11) {
+        console.log(30)
+    } else {
+        console.log(31)
+    }
+}
