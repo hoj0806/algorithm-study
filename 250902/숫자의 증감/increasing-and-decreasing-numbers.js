2ab224b0d34362ad2adfcc1db8fbba824866ee88
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 16:10:32 +0900

    Title: 숫자의 증감, Time: 71ms, Memory: 11MB, Status: Runtime Error - Codetree

---
+++ 250902/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(" ")
+
+let [str, num] = input
+
+let answer = ''
+
+if(str === 'A') {
+    for(let i = 1; i < Number(num); i+=) {
+        answer += i + " "
+    }
+} else {
+    for(let i = Number(num); i >= 1; i--) {
+        answer += i + " "
+    }
+}
+
+console.log(answer)
