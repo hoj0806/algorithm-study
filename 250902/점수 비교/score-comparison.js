83edf304873ac86f849c4ed064d277766f05d19b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-02 13:35:35 +0900

    Title: 점수 비교, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/점수 비교/score-comparison.js
+++ 250902/점수 비교/score-comparison.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split("\n")
 let[mathA, englishA] = input[0].split(" ").map(Number)
 let[mathB, englishB] = input[1].split(" ").map(Number)
 
-if(mathA > mathB && englishA && englishB) {
+if(mathA > mathB && englishA > englishB) {
     console.log(1)
 } else {
     console.log(0)
