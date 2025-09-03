2009c011bcedb157add727a62ef3f74b546db342
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 11:45:37 +0900

    Title: 1부터의 합, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/1부터의 합/sum-from-the-one.js
+++ 250903/1부터의 합/sum-from-the-one.js
@@ -6,11 +6,11 @@ let n = Number(input)
 let sum = 0
 
 for(let i = 1; i <= n; i++) {
+    sum+=i
     if(sum >= n) {
-        console.log(i - 1)
+        console.log(i)
         break
     }
-    sum+=i
 }
 
 
