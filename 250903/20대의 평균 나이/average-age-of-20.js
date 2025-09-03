bedf50de32aef0af46d7742d48f2541d32590f2a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 12:20:41 +0900

    Title: 20대의 평균 나이, Time: 61ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250903/20대의 평균 나이/average-age-of-20.js
+++ 250903/20대의 평균 나이/average-age-of-20.js
@@ -3,16 +3,15 @@ const input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
 
 let idx = 0
 let sum = 0
+
 while(true) {
     let age = input[idx]
 
-
     if(age >= 30) {
-        console.log((sum / idx).toFixed(2))
+      
         break
     } 
-        idx++
-        sum+=age
-    
 
+    idx++
+    sum+=age    
 }
