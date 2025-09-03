7513817004f3615e065a22413e4fa453e30dd229
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 12:55:07 +0900

    Title: 합성수 판별기, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250903/합성수 판별기/factorization-discriminator.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+let satisfied = false
+
+
+for(let i = 2; i < n; i++) {
+    if(n % i === 0) {
+        satisfied = true
+    }
+}
+
+
+if(satisfied) {
+    console.log('C')
+} else {
+    console.log('N')
+}
