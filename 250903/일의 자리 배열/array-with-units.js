ad8c9694f1c93c63a064e85310b5a5840a0600aa
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 15:54:06 +0900

    Title: 일의 자리 배열, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250903/일의 자리 배열/array-with-units.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let [a, b] = input
+
+let arr = [a, b]
+
+
+for(let i = 2; i < 10; i++) {
+    arr[i] = (arr[i - 1] + arr[i - 2]) % 10
+}
+
+console.log(arr.join(" "))
