a33e5dc3f695051ce9f01bf5a177c441d70391e7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 13:46:22 +0900

    Title: 별표 출력하기, Time: 71ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/별표 출력하기/print-star.js
+++ 250903/별표 출력하기/print-star.js
@@ -3,13 +3,16 @@ const input = fs.readFileSync(0).toString().trim()
 
 let n = Number(input)
 let cnt = 1;
+
 for(let i = 1; i <= (2 * n) - 1; i++) {
     let str = ''
     for(let j = 1; j <= cnt; j++) {
         str += "* "
     }
     console.log(str)
-    if(cnt < 5) {
+
+    
+    if(i < n) {
         cnt++
     } else {
         cnt--
