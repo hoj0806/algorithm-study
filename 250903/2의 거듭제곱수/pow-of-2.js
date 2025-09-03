740ce932ece205e1c754f8776a00db64fce96cb3
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 12:41:23 +0900

    Title: 2의 거듭제곱수, Time: 5021ms, Memory: 26MB, Status: Time Limit Exceed - Codetree

--- 250903/2의 거듭제곱수/pow-of-2.js
+++ 250903/2의 거듭제곱수/pow-of-2.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim()
 
-let n = Number(input)
+let n = BigInt(input)
 
-let r = 2
+let r = 2n
 let cnt = 1
 while(true) {
-    r*=2
+    r*=2n
     cnt+=1
 
     if(r === n) {
