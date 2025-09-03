95e52cc0b47f8a0a0111a7d8df595d7fa5359088
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-03 13:08:52 +0900

    Title: 정사각형 별표 출력하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250903/정사각형 별표 출력하기/print-stars-in-square.js
+++ 250903/정사각형 별표 출력하기/print-stars-in-square.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim()
 
-let n = Number(n)
+let n = Number(input)
 
 for(let i = 1; i <= n; i++) {
     let str = ''
-    for(let j = 1; j <= n; i++) {
+    for(let j = 1; j <= n; j++) {
         str += "*"
     }
     console.log(str)
