16c6c35183e3819e00516fcbfb108082bf1f2767
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-04 23:04:22 +0900

    Title: 연속부분수열일까, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250904/연속부분수열일까/contiguous-array-or-not.js
+++ 250904/연속부분수열일까/contiguous-array-or-not.js
@@ -5,7 +5,25 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 let [a, b] = input[0].split(" ").map(Number)
 let arr1 = input[1].split(" ").map(Number)
 let arr2 = input[2].split(" ").map(Number)
+let allMatched = true
 
-let idx = arr1.join("").indexOf(arr2.join(""))
+for(let i = 0; i < a - b + 1; i++) {
+    allMatched = true
+    for(let j = 0; j < b; j++) {
+        if(arr1[i+j] !== arr2[j]) {
+            allMatched = false
+            break
+        }
+    }
 
-console.log(idx === -1 ? "No" : "Yes")
+    if(allMatched) {
+        break
+    }
+}
+
+if(arr1.length > arr2.length) {
+    console.log("No")
+} else {
+console.log(allMatched === true ? "Yes" : "No")
+
+}
