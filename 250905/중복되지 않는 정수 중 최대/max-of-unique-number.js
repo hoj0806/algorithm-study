19205f8ddaff6c08cf395b7143d87a13c322450b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-05 00:21:21 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250905/중복되지 않는 정수 중 최대/max-of-unique-number.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n')
+
+let arr = input[1].split(" ").map(Number)
+
+let count = Array(10).fill(0)
+
+let answer = -1
+let maxValue = 0
+
+for(let i = 0; i < arr.length; i++) {
+    count[arr[i]]+=1
+}
+
+for(let i = 0; i < count.length; i++) {
+    if(count[i] === 1 && count[i] > maxValue) {
+        maxValue = i
+    }
+}
+
+console.log(maxValue === 0 ? -1 : maxValue)
