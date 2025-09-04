8b347427e0a14efb1db22e698d199c8f101e18ce
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-04 17:19:05 +0900

    Title: 알파벳 지우기, Time: 60ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250904/알파벳 지우기/remove-alphabet.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split('\n')
+
+let answer = 0
+
+for(let i = 0; i < input.length; i++) {
+    let str = input[i]
+    let k = ''
+    for(let j = 0; j < str.length; j++) {
+        if(str[j] >= "0" && str[j] <= "9") {
+            k+= str[j]
+        }
+    }
+
+    answer +=Number(k)
+}
+
+console.log(answer)
