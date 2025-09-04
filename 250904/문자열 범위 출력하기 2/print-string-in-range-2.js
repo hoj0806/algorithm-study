11fa192869e008a35c0f1e6401460628ecd6c03d
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-04 14:24:16 +0900

    Title: 문자열 범위 출력하기 2, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 250904/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 250904/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n')
 
+
 let str = input[0]
+let n = Number(input[1])
 
-
-console.log(str.slice(-5))
+console.log(str.slice(-(n)).split("").reverse().join(""))
