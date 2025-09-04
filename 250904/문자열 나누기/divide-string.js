2ac8fb971e8bcd2ed839272664f4a6c4bed577a4
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-04 14:43:30 +0900

    Title: 문자열 나누기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250904/문자열 나누기/divide-string.js
+++ 250904/문자열 나누기/divide-string.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n')
 
-let arr = input[1].split("")
+let str = input[1].split(" ").join("")
 
+let arr = []
 
-console.log(arr)
+for(let i = 0; i < str.length; i+=5) {
+    arr.push(str.slice(i, i + 5))
+}
+
+console.log(arr.join('\n'))
