b562a6a3dcd6c82466844b26ce180c9e3d2550a7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-05 00:58:39 +0900

    Title: 정수 직사각형, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 250905/정수 직사각형/rectangle-of-numbers.js
+++ 250905/정수 직사각형/rectangle-of-numbers.js
@@ -4,10 +4,11 @@ const input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 let [row, col] = input
 
 let matrix = Array(row).fill(0).map(() => Array(col).fill(0))
-
+let cnt = 1
 for(let i = 0; i < row; i++) {
     for(let j = 0; j < col; j++) {
-        matrix[i][j] = (3 * i) + (j + 1)
+        matrix[i][j] = cnt
+        cnt++
     }
     console.log(matrix[i].join(" "))
 }
