b399073188b22192d8f6e8a9aaf52b529f9f2940
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-07 00:39:21 +0900

    Title: 동전이 있는 위치, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/동전이 있는 위치/Where-coins-are-located.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split("\n")
+
+let [n, coin] = input[0].split(" ").map(Number)
+
+let matrix = Array(n).fill(0).map(() => Array(n).fill(0))
+
+for(let i = 1; i <= coin; i++) {
+    let [x, y] = input[i].split(" ").map(Number)
+    matrix[x-1][y-1] = 1
+}
+
+for(let i = 0; i < n; i++) {
+    console.log(matrix[i].join(" "))
+}
