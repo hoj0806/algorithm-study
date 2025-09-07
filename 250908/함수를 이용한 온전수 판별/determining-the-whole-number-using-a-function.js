31d0201ba80803a1b7818ec54199105c8f0ca4d8
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-08 00:46:10 +0900

    Title: 함수를 이용한 온전수 판별, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250908/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, b] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+
+function solution(n) {
+    if(n % 2 === 0) {
+        return false
+    }
+
+    if(n % 10 === 5) {
+        return false
+    }
+
+    if(n % 3 ===0 && n % 9 !== 0) {
+        return false
+    }
+
+    return true
+}
+
+let cnt = 0
+
+for(let i = a; i <= b; i++) {
+    if(solution(i)) {
+        cnt++
+    }
+}
+
+console.log(cnt)
