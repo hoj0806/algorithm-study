40b16465a5b8c521de0af815a00669267bba77e0
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-07 19:28:15 +0900

    Title: 모양대로 수 출력하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/모양대로 수 출력하기/print-out-numbers-in-specific-shape.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+for(let i = 1; i <= n; i++) {
+    let str = ''
+    for(let j = 1; j <= n; j++) {
+        if(i <= j) {
+            str += n - j + 1 + " "
+        } else {
+            str += "  "
+        }
+    }
+    console.log(str)
+}
