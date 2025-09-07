b6a6ffdec585522cb1bf47d49c8953458756d593
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-08 01:39:50 +0900

    Title: 2021년 날짜의 유무, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250908/2021년 날짜의 유무/with-or-without-2021.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [m, d] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+
+function solution(month, day) {
+    if(month === 2) {
+        if(day <= 28) {
+            return "Yes"
+        } else {
+            return "No"
+        }
+    } else {
+        if(month === 4 || month === 6 || month === 9 || month === 11) {
+            if(day <= 30) {
+                return "Yes"
+            } else {
+                return "No"
+            }
+        } else {
+            if(day <= 31) {
+                return "Yes"
+            } else {
+                return "No"
+            }
+        }
+    }
+}
+
+console.log(solution(m, d))
