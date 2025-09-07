65aec5b2ce7b58f617639717fa39ac3298d8a3cc
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-07 18:27:44 +0900

    Title: 정사각형 출력, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/정사각형 출력/print-square.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+let cnt = 1
+
+for(let i = 1; i <= n; i++) {
+    let str = ''    
+    for(let j = 1; j <= n; j++) {
+        str += cnt + " "
+        cnt++
+    }
+    console.log(str)
+}
