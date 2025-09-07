9c0df3388d2ef56fb9c47a5ab07b97204c1a59c6
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-07 19:05:34 +0900

    Title: 정상적인 구구단, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/정상적인 구구단/normal-multiple.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+for(let i = 1; i <= n; i++) {
+    let str = ''
+    for(let j = 1; j <= n; j++) {
+        str += `${i} * ${j} = ${i * j}` 
+        if(j !== n) {
+            str += ", "
+        }
+    }
+    console.log(str)
+}
