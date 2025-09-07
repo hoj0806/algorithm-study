b111e7f03aef2e8977c6b4b7e0d3a2f3d8a5b589
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-07 20:13:43 +0900

    Title: 알파벳 출력하기 2, Time: 61ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/알파벳 출력하기 2/print-alphabet-2.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim()
+
+let n = Number(input)
+
+let number = 65
+
+for(let i = 1; i <= n; i++) {
+    let str = ''
+    for(let j = 1; j <=n; j++) {
+        if(i <= j) {
+            str += String.fromCharCode(number) + " "
+            number++
+            if(number === 91) number = 65
+        } else {
+            str += "  "
+        }
+    }
+    console.log(str)
+}
