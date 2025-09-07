b1f89e4f85a15d7a8252cf1b959b73563dae0df0
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-07 19:11:57 +0900

    Title: 구구단 만들기 2, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250907/구구단 만들기 2/print-multiplication-table-2.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let [a, b] = input
+
+for(let i = 2; i <= 8; i+=2) {
+    let str = ''
+    for(let j = b ; j >= a; j--) {
+        str += `${j} * ${i} = ${j * i}`
+        if(j !== a) {
+            str += " / "
+        }
+    }
+    console.log(str)
+}
