b7b98137b30b08756674a921026dc94d3283ec5e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-09 00:18:19 +0900

    Title: Next Level, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/Next Level/next-level.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [user_id, levelStr] = input[0].split(' ');
+const level = parseInt(levelStr);
+
+// Please Write your code here.
+
+class User {
+    constructor(id = "codetree", level = 10) {
+        this.id = id
+        this.level = level
+    }
+}
+
+
+let user1 = new User()
+
+console.log(`user ${user1.id} lv ${user1.level}`)
+user1.id = "hello"
+user1.level = 28
+console.log(`user ${user1.id} lv ${user1.level}`)
