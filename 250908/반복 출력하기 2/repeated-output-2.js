2882e2e406abaf4206da03bfc87cac03b4cc142f
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-08 19:42:54 +0900

    Title: 반복 출력하기 2, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250908/반복 출력하기 2/repeated-output-2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+
+
+function printHelloWorld(n) {
+    if(n === 0) {
+        return
+    }
+    console.log("HelloWorld")
+    return printHelloWorld(n - 1)
+}
+
+printHelloWorld(n)
