4c21c40e4331c21d1624ac9ba441afe6f4882880
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-09 17:58:45 +0900

    Title: DateTime to DateTime, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/DateTime to DateTime/datetime-to-datetime.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [A, B, C] = input[0].split(' ').map(Number);
+
+// Please write your code here.
+
+
+let answer = 0
+
+if(A < 11 || B < 11 || C < 11) {
+    console.log(-1)
+} else {
+    if(A !== 11) {
+        answer += (A - 11) * 1440
+    }
+    let a = ((B * 60) + C)
+    let b = ((11 * 60) + 11)
+    answer += (a - b)
+    console.log(answer)
+}
+
