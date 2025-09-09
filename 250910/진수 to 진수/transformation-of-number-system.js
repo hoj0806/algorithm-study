5d3b289ab2345ce328a33526948278c5eb24f218
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-10 02:47:36 +0900

    Title: 진수 to 진수, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 250910/진수 to 진수/transformation-of-number-system.js
+++ 250910/진수 to 진수/transformation-of-number-system.js
@@ -7,5 +7,21 @@ const n = input[1];
 
 let sum = 0
 
-let arr = n.split(" ").map(Number)
-console.log(arr)
+let arr = n.split("").map(Number)
+
+for(let i = 0; i < arr.length; i++) {
+    sum = sum * a + arr[i]
+}
+
+let answer = []
+
+while(true) {
+    if(sum < b) {
+        answer.push(sum)
+        break
+    }
+    answer.push(sum % b)
+    sum = parseInt(sum / b)
+}
+
+console.log(answer.reverse().join(""))
