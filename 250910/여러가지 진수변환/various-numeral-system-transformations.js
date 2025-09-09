0446185b322ce2e3f27fd414b188e93623b48a03
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-10 02:27:07 +0900

    Title: 여러가지 진수변환, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250910/여러가지 진수변환/various-numeral-system-transformations.js
+++ 250910/여러가지 진수변환/various-numeral-system-transformations.js
@@ -3,4 +3,4 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let [n, b] = input[0].split(' ').map(Number);
 
 // Please Write your code here.
-console.log(111 % 4)
+console.log(111 / 4)
