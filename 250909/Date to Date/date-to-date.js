40dd21875733d15cdedd8b2777866d7f21a03174
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-09 17:47:05 +0900

    Title: Date to Date, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250909/Date to Date/date-to-date.js
+++ 250909/Date to Date/date-to-date.js
@@ -1,23 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+let [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 
 // Please Write your code here.
 
 let days = [0, 31, 28, 31, 30, 31, 30 ,31 ,31, 30 ,31, 30, 31]
 
-let a = 0
-let b = 0
+let answer = 1
 
-for(let i = 1; i <= m1; i++) {
-    a+=days[i]
+while(true) {
+    if(m1 === m2 && d1 === d2) {
+        break
+    }
+
+    d1++
+    if(d1 > days[m1]) {
+        m1+=1
+        d1 = 1
+    }
+    answer+=1
 }
-a+=d1
 
-for(let i = 1; i <= m2; i++) {
-    b+=days[i]
-}
-b+= d2
+console.log(answer)
 
-console.log(b - a)
