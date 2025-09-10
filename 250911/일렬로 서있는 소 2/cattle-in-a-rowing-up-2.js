1ba91f42fbb8076f5dfdf06c799e25d9e5a509ee
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-11 02:01:46 +0900

    Title: 일렬로 서있는 소 2, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 250911/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
+++ 250911/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
@@ -10,7 +10,7 @@ let sum = 0
 for(let i = 0; i < arr.length; i++) {
     for(let j = i + 1; j < arr.length; j++) {
         for(let k = j + 1; k < arr.length; k++) {
-            if(arr[i] <= arr[j] && arr[j] < arr[k] && arr[i] < arr[k]) {
+            if(arr[i] <= arr[j] && arr[j] <= arr[k] && arr[i] <= arr[k]) {
                 sum++
             }
         }
