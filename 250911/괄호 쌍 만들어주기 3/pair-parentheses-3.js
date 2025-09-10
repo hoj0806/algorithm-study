3eb18d63b90797950280e4d9e4fab93532efa770
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-11 01:57:51 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250911/괄호 쌍 만들어주기 3/pair-parentheses-3.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const A = input[0];
+
+// Please Write your code here.
+
+let sum = 0
+
+for(let i = 0; i < A.length; i++) {
+    for(let j = i + 1; j < A.length; j++) {
+        if(A[i] === "(" && A[j] === ")") sum++
+    }
+}
+
+console.log(sum)
