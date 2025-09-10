87e1b459983c3ae671a0e14d1e02945e8391f3b4
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-11 08:56:50 +0900

    Title: 만나는 그 순간, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250911/만나는 그 순간/the-moment-we-meet.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m] = input[0].split(' ').map(Number);
+let line = 1;
+const movesA = [];
+for (let i = 0; i < n; i++) {
+    const [d, t] = input[line++].split(' ');
+    movesA.push([d, Number(t)]);
+}
+const movesB = [];
+for (let i = 0; i < m; i++) {
+    const [d, t] = input[line++].split(' ');
+    movesB.push([d, Number(t)]);
+}
+
+// Please Write your code here.
+
+let x = [0]
+let y = [0]
+let startX = 0
+let startY = 0
+
+for(let i = 0; i < movesA.length; i++) {
+    let [c, n] = movesA[i]
+    
+    if(c === 'R') {
+        for(let j = 0; j <= n - 1; j++) {
+            startX++
+            x.push(startX)
+        }
+    } else {
+        for(let j = 0; j <= n - 1; j++) {
+            startX--
+            x.push(startX)
+        }
+    }
+}
+
+for(let i = 0; i < movesB.length; i++) {
+    let [c, n] = movesB[i]
+    
+    if(c === 'R') {
+        for(let j = 0; j <= n - 1; j++) {
+            startY++
+            y.push(startY)
+        }
+    } else {
+        for(let j = 0; j <= n - 1; j++) {
+            startY--
+            y.push(startY)
+        }
+    }
+}
+
+for(let i = 1; i < x.length; i++) {
+    if(x[i] === y[i]) {
+        console.log(i)
+        break
+    }
+}
+
