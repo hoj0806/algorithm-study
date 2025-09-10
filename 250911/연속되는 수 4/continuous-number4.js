e82f92e9b82517ece64b57053ca758f4f69c30ca
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-11 08:20:02 +0900

    Title: 연속되는 수 4, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250911/연속되는 수 4/continuous-number4.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n).map(Number);
+// Please Write your code here.
+
+let r = []
+
+let cnt = 0
+
+for(let i = 0; i < arr.length; i++) {
+    if(i === 0) {
+        cnt++
+    } else {
+        if(arr[i] > arr[i - 1]) {
+            cnt++
+        } else {
+            r.push(cnt)
+            cnt = 1
+        }
+    }
+}
+
+r.push(cnt)
+
+console.log(Math.max(...r))
