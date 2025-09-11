86b33e10163739d3881e9ae9288eda0772eda11d
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-11 20:01:34 +0900

    Title: 좌우로 움직이는 로봇, Time: 77ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250911/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const movesA = input.slice(1, 1 + n);
+const movesB = input.slice(1 + n, 1 + n + m);
+
+// Please Write your code here.
+
+let aRobot = [0]
+let bRobot = [0]
+let aStart = 0
+let bStart = 0
+
+for(let i = 0; i < movesA.length; i++) {
+    let [n, c] = movesA[i].split(" ")
+    n = Number(n)
+    
+    if(c === 'R') {
+        for(let i = 1; i <= n; i++) {
+            aStart++
+            aRobot.push(aStart)
+        }
+    } else {
+        for(let i = 1; i <= n; i++) {
+            aStart--
+            aRobot.push(aStart)
+        }
+    }
+}
+
+for(let i = 0; i < movesB.length; i++) {
+    let [n, c] = movesB[i].split(" ")
+    n = Number(n)
+    
+    if(c === 'R') {
+        for(let i = 1; i <= n; i++) {
+            bStart++
+            bRobot.push(bStart)
+        }
+    } else {
+        for(let i = 1; i <= n; i++) {
+            bStart--
+            bRobot.push(bStart)
+        }
+    }
+}
+
+console.log(aRobot)
+console.log(bRobot)
+
+let answer = []
+let isSame = false
+for(let i = 1; i < Math.max(aRobot.length, bRobot.length); i++) {
+    console.log(aRobot[i], bRobot[i])
+    if(aRobot[i] !== undefined && bRobot[i] !== undefined) {
+        if(aRobot[i] === bRobot[i]) {
+            if(isSame === false) answer.push(i)
+            isSame = true 
+        } else {
+            isSame = false
+        }
+    }
+}
+
+console.log(answer)
