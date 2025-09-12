5d7ee0a7099e26f2085de4b54df2c7fb1d5d5f3b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-12 17:32:56 +0900

    Title: 비둘기와 전기줄, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 250912/비둘기와 전기줄/pigeons-and-electric-cords.js
+++ 250912/비둘기와 전기줄/pigeons-and-electric-cords.js
@@ -4,11 +4,8 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const movements = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
-// Please Write your code here.
 
 let arr = Array(11).fill("")
-
-console.log(arr)
 
 for(let i = 0; i < movements.length; i++) {
     let m = movements[i]
@@ -18,5 +15,11 @@ for(let i = 0; i < movements.length; i++) {
 let answer = 0
 let s = arr.filter((i) => i !== "" && i.length > 1)
 
-// for(let i = 0)
-console.log(s)
+for(let i = 0; i < s.length; i++) {
+    let str = s[i]
+    for(let j = 0; j < str.length - 1; j++) {
+        if(str[j] !== str[j+1]) answer++
+    }
+}
+
+console.log(answer)
