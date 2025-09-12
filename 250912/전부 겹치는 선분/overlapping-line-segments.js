81dfdfb9a0ba16a04fb1b7ba59f0e1938f30bf4d
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-12 17:07:03 +0900

    Title: 전부 겹치는 선분, Time: 79ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250912/전부 겹치는 선분/overlapping-line-segments.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const segments = [];
+
+let answer = 'Yes'
+
+let line = input[1]
+for (let i = 2; i <= n; i++) {
+    let s = input[i]
+
+    let [a1, a2] = line.split(" ").map(Number)
+    let [b1, b2] = s.split(" ").map(Number)
+
+    if(a2 < b1 || b2 < a1) {
+        answer = 'No'
+        break
+    } else {
+        if(a1 > b1) {
+            line = [b1, a2].join(" ")
+        } else if(b1 > a1) {
+            line = [a1, b2].join(" ")
+        } else {
+            line = [a1, a2].join(" ")
+        }
+    }
+
+}
+
+console.log(answer)
