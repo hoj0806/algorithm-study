ac71ada6a600f5753aa58a8521439543fce1687c
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-15 03:18:23 +0900

    Title: 정수 명령 처리 5, Time: 110ms, Memory: 13MB, Status: Passed - Codetree

--- 250915/정수 명령 처리 5/process-numeric-commands-5.js
+++ 250915/정수 명령 처리 5/process-numeric-commands-5.js
@@ -7,9 +7,19 @@ const commands = input.slice(1);
 // Please Write your code here.
 
 
-let arr = []
 
+
+let arr = []
 for(let i = 0; i < commands.length; i++) {
-    let arr = commands[i].split(" ")
-    console.log(arr)
+
+    let c = commands[i].split(" ")
+    if(c[0] === 'push_back') {
+        arr.push(Number(c[1]))
+    } else if(c[0] === 'pop_back') {
+        arr.pop()
+    } else if(c[0] === 'size') {
+        console.log(arr.length)
+    } else if(c[0] === 'get') {
+        console.log(arr[Number(c[1]) - 1])
+    }
 }
