22675ef1f1a36b0909582cab2388d74d75ddd06d
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-15 06:16:27 +0900

    Title: 움직이는 블록, Time: 5034ms, Memory: 10MB, Status: Time Limit Exceed - Codetree

--- 250915/움직이는 블록/moving-block.js
+++ 250915/움직이는 블록/moving-block.js
@@ -8,17 +8,28 @@ const blocks = input.slice(1, n + 1).map(Number);
 
 let cnt = 0
 while(true) {
-    if(new Set(blocks).size === 1) {
-        break
+    let allSame = true
+   let first = blocks[0]
+    for(let i = 1; i < blocks.length; i++) {
+            if(first !== blocks[i]) {
+                allSame = false
+                break
+            }
     }
 
+    if(allSame === true) {
+        break
+    }
     let max = Math.max(...blocks)
     let min = Math.min(...blocks)
+
     let maxIndex = blocks.indexOf(max)
     let minIndex = blocks.indexOf(min)
+
     blocks[maxIndex]-=1
     blocks[minIndex]+=1
     cnt++
+    
 }
 
 console.log(cnt)
