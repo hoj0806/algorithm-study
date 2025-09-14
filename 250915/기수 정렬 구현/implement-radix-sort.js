a9d20527ad9e1041292e24ddc1358adec2e03507
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-15 01:25:53 +0900

    Title: 기수 정렬 구현, Time: 5021ms, Memory: 29MB, Status: Time Limit Exceed - Codetree

--- 250915/기수 정렬 구현/implement-radix-sort.js
+++ 250915/기수 정렬 구현/implement-radix-sort.js
@@ -13,8 +13,25 @@ function getMaxLength(arr) {
 
 function radix_sort(arr) {
     let maxLength = getMaxLength(arr)
-    for(let pos = 0; i < maxLength; i++) {
-        let radix_arr = new Array({ length : 10}, () => [])
-        
+    for(let pos = 0; pos < maxLength; pos++) {
+        let radix_arr = Array(10).fill(0).map(() => [])
+
+        for(let i = 0; i < arr.length; i++) {
+            let num = arr[i]
+            let digit = Math.floor(num / Math.pow(10, pos)) % 10
+            radix_arr[digit].push(num)
+        }
+
+        let stored_arr = []
+        for(let j = 0; j < 10; j++) {
+            for(let k = 0; k < radix_arr[j].length; k++) {
+                stored_arr.push(radix_arr[j][k])
+            }
+        }
+
+        arr = stored_arr
     }
+    console.log(arr.join(" "))
 }
+
+radix_sort(arr)
