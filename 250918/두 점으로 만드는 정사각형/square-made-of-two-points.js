e2771c601f4c27a41300b883588a6d159ef3b9a7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-18 00:45:52 +0900

    Title: 두 점으로 만드는 정사각형, Time: 60ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250918/두 점으로 만드는 정사각형/square-made-of-two-points.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
+const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+
+let maxX = Math.max(x1, x2, a1, a2)
+let minX = Math.min(x1, x2, a1, a2)
+let maxY = Math.max(y1, y2, b1, b2)
+let minY = Math.min(y1, y2, b1, b2)
+
+let line = Math.max((maxX - minX), (maY - minY))
+
+console.log(line**2)
