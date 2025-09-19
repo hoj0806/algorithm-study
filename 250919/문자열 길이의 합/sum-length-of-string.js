7151a1fbe1ef529d25559307812d2f28cbe50b7e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-19 17:48:50 +0900

    Title: 문자열 길이의 합, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250919/문자열 길이의 합/sum-length-of-string.js
+++ 250919/문자열 길이의 합/sum-length-of-string.js
@@ -9,7 +9,7 @@ let sum = 0
 for(let i = 1; i <= n; i++) {
     let str = input[i]
     sum += input[i].length
-    if(str.indexOf("a") !== -1) {
+    if(str.indexOf("a") === 0) {
         cnt+=1
     }
 }
