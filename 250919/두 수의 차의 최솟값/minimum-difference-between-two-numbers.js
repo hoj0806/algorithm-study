a58c267bb02188b8f974544e449e501c5d7c2819
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-19 17:22:18 +0900

    Title: 두 수의 차의 최솟값, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250919/두 수의 차의 최솟값/minimum-difference-between-two-numbers.js
+++ 250919/두 수의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -5,13 +5,11 @@ let arr = input[1].split(" ").map(Number)
 
 let min = Number.MAX_SAFE_INTEGER
 
-for(let i = 0; i < arr.length; i++) {
-    for(let j = i + 1; j < arr.length; j++) {
-        let diff = Math.abs(arr[i] - arr[j])
-        if(diff < min) {
+for(let i = 1; i < arr.length; i++) {
+     let diff = Math.abs(arr[i] - arr[i - 1])
+     if(diff < min) {
             min = diff
         }
-    }
 }
 
 console.log(min)
