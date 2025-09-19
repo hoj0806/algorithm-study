3b2d0085cc53f433cdec1d625a01be48c1db5c1d
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-19 15:51:58 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250919/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
+++ 250919/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -8,12 +8,22 @@ let n = Number(input)
 for(let i = 1; i <= n * 2 - 1; i++) {
     let str = ''
 
-    for(let j = 1; j <= n; j++) {
-       if(i <= n) {
-        str += "@ "
-       }
-    }
-
-    console.log(str)
+   for(let j = 1; j <= n; j++) {
+        if(i <= n) {
+            if(i + j > n) {
+                str += "@ "
+            } else {
+                str += "  "
+            }
+        } else {
+            if(Math.abs(3 - i) + j <= n) {
+                str += "@ "
+            } else {
+                str += "  "
+            }
+           
+        }
+   }
     
+console.log(str)
 }
