2e13eb0c085059699760eb3ff2691a716d840719
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-20 15:11:45 +0900

    Title: 왔다 갔던 구역 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250920/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1, n + 1);
+
+// Please Write your code here.
+
+let segments = Array(3000).fill(0)
+let startIndex = 1500
+
+for(let i = 0; i< commands.length; i++) {
+    let [n, c] = commands[i].split(" ")
+   
+    if(c === 'R') {
+        for(let i = startIndex; i <= startIndex + Number(n) - 1; i++) {
+                segments[i]++
+        }
+            startIndex+=Number(n) 
+    } else {
+        for(let i = startIndex; i >= startIndex - Number(n) + 1; i--) {
+            segments[i]++
+        }
+        startIndex-=Number(n) 
+    }
+}
+
+console.log(segments.filter((i) => i > 1).length)
+
