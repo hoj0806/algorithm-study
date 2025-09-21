bc586f274844bbbdcd5cbc009576c91783ce799c
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-21 16:17:54 +0900

    Title: 그래프 탐색, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250921/그래프 탐색/graph-traversal.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+
+let graph = Array(n + 1).fill(0).map(() => Array(n + 1).fill(0))
+
+for (let i = 1; i <= m; i++) {
+    let [x, y] = input[i].split(" ").map(Number)
+    graph[x][y] = 1
+    graph[y][x] = 1
+}
+
+let visited = Array(n + 1).fill(false)
+
+function dfs(vertex) {
+    for(let currV = 1; currV <= n; currV++) {
+        if(graph[vertex][currV] && !visited[currV]) {
+            visited[currV] = true
+            dfs(currV)
+        }
+    }
+}
+
+dfs(1)
+
+console.log(visited.filter((i) => i === true).length - 1)
