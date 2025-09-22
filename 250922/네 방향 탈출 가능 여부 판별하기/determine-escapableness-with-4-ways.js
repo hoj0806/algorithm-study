4d48f5323e8d0aeff25a0b29cd47d71dad5a9ec4
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-22 11:48:57 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 74ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250922/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+let graph = Array(n).fill(0).map(() => [])
+
+console.log(graph)
