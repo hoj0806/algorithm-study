7b2f41f76ac9ea6942b0306c16e697a8122ba7f9
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-22 12:09:27 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 89ms, Memory: 13MB, Status: Passed - Codetree

--- 250922/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.js
+++ 250922/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.js
@@ -6,8 +6,7 @@ const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 
-let graph = Array(n + 1).fill(0).map(() => Array(m + 1).fill(0))
-let visited = Array(n + 1).fill(false)
+let visited = Array(n).fill(false).map(() => Array(m).fill(false))
 
 class Queue {
     constructor() {
@@ -30,4 +29,43 @@ class Queue {
     }
 }
 
-console.log(graph)
+let q = new Queue()
+
+function isRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < m
+}
+
+function canGo(x, y) {
+    if(!isRange(x, y)) {
+        return false
+    }
+
+    if(visited[x][y] || grid[x][y] === 0) {
+        return false
+    }
+
+    return true
+}
+function bfs() {
+    let dx = [0, 1, 0, -1]
+    let dy = [1, 0, -1, 0]
+
+    while(!q.empty()) {
+        let currV = q.pop()
+        let [x, y] = currV
+        for(let i = 0; i < dx.length; i++) {
+            let newX = x + dx[i]
+            let newY = y + dy[i]
+                if(canGo(newX, newY)) {
+                    visited[newX][newY] = true
+            q.push([newX, newY])
+        }
+    }
+    }
+  
+}
+
+q.push([0, 0])
+visited[0][0] = true
+bfs()
+console.log(+visited[n-1][m-1])
