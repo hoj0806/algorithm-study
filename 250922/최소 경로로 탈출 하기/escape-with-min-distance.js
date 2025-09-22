548c34c0b53337431ad92d3b8612910d1fdeff22
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-22 12:56:44 +0900

    Title: 최소 경로로 탈출 하기, Time: 85ms, Memory: 13MB, Status: Passed - Codetree

--- 250922/최소 경로로 탈출 하기/escape-with-min-distance.js
+++ 250922/최소 경로로 탈출 하기/escape-with-min-distance.js
@@ -4,13 +4,11 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
-// Please Write your code here.
-
 class Queue {
     constructor() {
         this.q = []
         this.head = -1
-        this.tail = -1
+        this.tail = -1 
     }
 
     push(item) {
@@ -27,50 +25,41 @@ class Queue {
     }
 }
 
-let step = Array(n).fill(0).map(() => Array(m).fill(0))
-let visited = Array(n).fill(false).map(() => Array(m).fill(false))
+let step = Array(n).fill(0).map(() => Array(m).fill(0));
+let visited = Array(n).fill(false).map(() => Array(m).fill(false));
+let q = new Queue();
 
 function isRange(x, y) {
-    return 0 <= x && x < n && 0 <= y && y < m
+    return 0 <= x && x < n && 0 <= y && y < m;
 }
 
 function canGo(x, y) {
-    if(!isRange(x, y)) {
-        return false
-    }
-
-    if(visited[x][y] || grid[x][y] === 0) {
-        return false
-    }
-
-    return true
+    return isRange(x, y) && !visited[x][y] && grid[x][y] === 1;
 }
 
-function go(...[x, y, z]) {
-    step[x][y] = z
-    q.push([x, y])
-    visited[x][y] = true
+function go(x, y, z) {
+    step[x][y] = z;
+    visited[x][y] = true;
+    q.push([x, y, z]); 
 }
 
 function bfs() {
-    let dx = [0, 1, 0, -1]
-    let dy = [1, 0, -1 , 0]
+    let dx = [0, 1, 0, -1];
+    let dy = [1, 0, -1, 0];
 
     while(!q.empty()) {
-        let [x, y, z] = q.pop()
-
-        for(let i = 0; i < dx.length; i++) {
-            let newX = x + dx[i]
-            let newY = y + dy[i]
+        let [x, y, z] = q.pop();
+        for(let i = 0; i < 4; i++) {
+            let newX = x + dx[i];
+            let newY = y + dy[i];
             if(canGo(newX, newY)) {
-                go(newX, newY, step[x][y] + 1)
+                go(newX, newY, z + 1); // step 값을 올려서 큐에 넣기
             }
         }
     }
 }
 
+go(0, 0, 0);
+bfs();
 
-let q = new Queue()
-q.push([0, 0, 0])
-bfs()
-console.log(step[n-1][m-1] === 0 ? -1 : step[n-1][m-1])
+console.log(step[n-1][m-1] === 0 ? -1 : step[n-1][m-1]);
