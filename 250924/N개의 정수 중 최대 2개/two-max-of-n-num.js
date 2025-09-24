a0f44455557f9aed74020eaf9fe123f74fffc75b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-24 16:22:52 +0900

    Title: N개의 정수 중 최대 2개, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250924/N개의 정수 중 최대 2개/two-max-of-n-num.js
+++ 250924/N개의 정수 중 최대 2개/two-max-of-n-num.js
@@ -5,7 +5,19 @@ let r = input[1].split(" ").map(Number)
 
 let max = Number.MIN_SAFE_INTEGER
 let nextMax = Number.MIN_SAFE_INTEGER
-
+let findMax = false
 for(let i = 0; i < r.length; i++) {
-  console.log(r)
+  if(r[i] > max) {
+    if(findMax === false) {
+         max = r[i]
+        nextMax = r[i]
+        findMax = true
+    } else {
+      nextMax = max
+      max = r[i]
+    }
+ 
+  }
 }
+
+console.log(max, findMax)
