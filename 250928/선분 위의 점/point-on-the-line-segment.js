8f659e1d287df640629b3cc58d87fbc903043124
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-28 03:38:12 +0900

    Title: 선분 위의 점, Time: 448ms, Memory: 160MB, Status: Memory Limit Exceed - Codetree

--- 250928/선분 위의 점/point-on-the-line-segment.js
+++ 250928/선분 위의 점/point-on-the-line-segment.js
@@ -7,13 +7,54 @@ const queries = input.slice(2).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 
-console.log(points)
+function lower_bound(arr, target) {
+    let left = 0
+    let right = arr.length
+    
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2)
 
+        if(arr[mid] >= target) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return left
+}
+
+function upper_bound(arr, target) {
+    let left = 0
+    let right = arr.length
+    
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if(arr[mid] > target) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return left
+}
+
+let answer = []
 for(let i = 0; i < queries.length; i++) {
     let arr = []
     let [start, end] = queries[i]
     for(let j = start; j <= end; j++) {
         arr.push(j)
     }
-    console.log(arr)
+    let count = 0
+    for(let z = 0; z < points.length; z++) {
+        let ub = upper_bound(arr, points[z])
+        let lb = lower_bound(arr, points[z])
+        count += ub - lb
+    }
+    answer.push(count)
 }
+
+console.log(answer.join('\n'))
