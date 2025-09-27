b9532e29ed67e1a8a74062f39699af4b0f5c16ac
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-28 02:51:33 +0900

    Title: 숫자 빠르게 찾기, Time: 807ms, Memory: 37MB, Status: Passed - Codetree

--- 250928/숫자 빠르게 찾기/find-number-fast.js
+++ 250928/숫자 빠르게 찾기/find-number-fast.js
@@ -11,7 +11,9 @@ function binary_search(arr, target) {
     let left = 0
     let right = arr.length - 1
     while(left <= right) {
-        if(arr[mid] === target) return mid
+        let mid = Math.floor((left + right) / 2)
+
+        if(arr[mid] === target) return mid + 1
 
         if(arr[mid] > target) {
             right = mid - 1
