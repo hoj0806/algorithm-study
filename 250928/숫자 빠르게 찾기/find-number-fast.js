6261a176c1ff2874960036651de96531c1a1b34e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-09-28 02:50:24 +0900

    Title: 숫자 빠르게 찾기, Time: 63ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250928/숫자 빠르게 찾기/find-number-fast.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const numbers = input[1].split(' ').map(Number);
+const queries = input.slice(2, 2 + m).map(Number);
+
+// Please Write your code here.
+
+function binary_search(arr, target) {
+    let left = 0
+    let right = arr.length - 1
+    while(left <= right) {
+        if(arr[mid] === target) return mid
+
+        if(arr[mid] > target) {
+            right = mid - 1
+        } else if(arr[mid] < target) {
+            left = mid + 1
+        }
+    }
+
+    return -1
+}
+
+
+for(let i = 0; i < queries.length; i++) {
+    console.log(binary_search(arr, queries[i]))
+}
