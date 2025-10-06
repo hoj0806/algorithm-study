646455fe057eb4dcaeb7c85070a22b24b7a63c11
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-06 15:00:27 +0900

    Title: 운행 되고 있는 시간, Time: 73ms, Memory: 12MB, Status: Passed - Codetree

--- 251006/운행 되고 있는 시간/hours-in-service.js
+++ 251006/운행 되고 있는 시간/hours-in-service.js
@@ -4,25 +4,27 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const n = Number(input[0]);
 const segments = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
-// Please Write your code here.
-let answer = 0
+// Please Write your code here
+
+let arr = Array(1000).fill(0)
 
 for(let i = 0; i < segments.length; i++) {
-    for(let j = i + 1; j < segments.length; j++) {
-        let [s1, e1] = segments[i]
-        let [s2, e2] = segments[j]
-        let arr = [[s1, e1], [s2, e2]]
-        let table = Array(1000).fill(0)
-        for(let k = 0; k < arr.length; k++) {
-            let [start , end] = arr[k]
-            for(let l = start + 1; l <= end; l++) {
-               table[l]+=1
-            }
-        }
-        answer = Math.max(answer, 
-        table.filter((i) => i !== 0).length
-        )
+    let [s, e] = segments[i]
+    for(let j = s + 1; j <= e; j++) {
+        arr[j]+=1
     }
+}
+
+
+let answer = -1
+
+for(let i = 0; i < segments.length; i++) {
+    let [s, e] = segments[i]
+    let z = arr.slice()
+    for(let j = s + 1; j <=e ; j++) {
+        z[j]-=1
+    }
+    answer = Math.max(answer, z.filter((i) => i!==0).length)
 }
 
 console.log(answer)
