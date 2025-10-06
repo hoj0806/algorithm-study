11da2ab0a52743523bbfcc2b7f297c879e937b13
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-06 13:04:11 +0900

    Title: 개발팀의 능력, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251006/개발팀의 능력/the-capabilities-of-the-development-team.js
+++ 251006/개발팀의 능력/the-capabilities-of-the-development-team.js
@@ -9,15 +9,20 @@ let answer = 10000
 let sum = abilities.reduce((a, c) => a + c , 0)
 for(let i = 0; i < abilities.length; i++) {
     for(let j = i + 1; j < abilities.length; j++) {
-          let x = abilities[i] + abilities[j]
         for(let k = j + 1; k < abilities.length; k++) {
-            let y = abilities[k]
-            let z = sum - (x + y)
-            if(x !== y && y !== z && x !== z) {
-                let max = Math.max(x, y, z)
-                let min = Math.min(x, y, z)
+            let arr = [abilities[i] ,abilities[j] ,abilities[k]]
+            let arrS = arr.reduce((a, c) => a + c, 0)
+            for(let p = 0; p < arr.length; p++) {
+                let p1 = arr[p]
+                let p2 = arrS - p1
+                let p3 = sum - (p1 + p2)
+                if(p1 !== p2 && p2 !== p3 && p1 !== p3) {
+                let max = Math.max(p1, p2, p3)
+                let min = Math.min(p1, p2, p3)
                 answer = Math.min(answer, Math.abs(max - min))
             }
+            }
+           
         }
     }
 }
