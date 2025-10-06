cbe27addc07df2b163e3edd396f3f87421729063
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-06 17:04:28 +0900

    Title: 데이터센터의 온도 조정 2, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251006/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.js
+++ 251006/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.js
@@ -5,19 +5,9 @@ const [n, c, g, h] = input[0].split(' ').map(Number);
 
 let k = input.slice(1).map(i => i.split(" ").map(Number))
 
-
-let arr = []
-for(let i = 0; i < k.length; i++) {
-    let [s, e] = k[i]
-    for(let j = s; j <= e; j++) {
-        arr.push(j)
-    }
-}
-
-
 let answer = 0
-for(let i = 0; i < arr.length; i++) {
-    let t = arr[i]
+for(let i = 0; i <= 1000; i++) {
+    let t = i
     let sum = 0
     for(let j = 0; j < k.length; j++) {
         let [s, e] = k[j]
