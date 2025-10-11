42faacc79acad503eace689fe7d28e5b56064f33
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-11 17:52:34 +0900

    Title: 두 번째로 작은 수의 위치, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251011/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.js
+++ 251011/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.js
@@ -7,9 +7,11 @@ const a = input[1].split(" ").map(Number);
 // Please Write your code here.
 
 
-let s = [...new Set(a.sort((a, b) => a - b))]
+let s = [...new Set(a.slice().sort((a, b) => a - b))]
 
 // s의 길이가 1일경우 불가능, s[1]의 필터링 개수가 1개보다 많을 경우
+
+
 
 if(s.length === 1 || a.filter(i => i === s[1]).length > 1) {
     console.log(-1)
