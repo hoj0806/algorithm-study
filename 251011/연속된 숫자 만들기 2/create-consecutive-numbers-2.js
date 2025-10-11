694c5be2abb608e799cd16e0b0cab32d8dadacdc
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-11 16:56:16 +0900

    Title: 연속된 숫자 만들기 2, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251011/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const arr = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+
+
+let x = Math.abs(arr[0] - arr[1])
+let y = Math.abs(arr[1] - arr[2])
+
+if(x === 1 && y === 1) {
+    console.log(0)
+} else if(x === 2 || y === 2) {
+    console.log(1)
+} else {
+    console.log(2)
+}
