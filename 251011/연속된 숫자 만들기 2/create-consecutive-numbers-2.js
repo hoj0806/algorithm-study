da03fe9d3d38d81838c32137136db0a44160c4e0
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-11 17:05:14 +0900

    Title: 연속된 숫자 만들기 2, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251011/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
+++ 251011/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
@@ -7,10 +7,11 @@ const arr = input[0].split(" ").map(Number);
 
 let x = Math.abs(arr[0] - arr[1])
 let y = Math.abs(arr[0] - arr[2])
+let z = Math.abs(arr[1] - arr[2])
 
 if((x === 1 && y === 2) || (x === 2 && y === 1) | (x === 1 && y === 1)) {
     console.log(0)
-} else if(x === 2 || y === 2) {
+} else if(x === 2 || z === 2) {
     console.log(1)
 } else {
     console.log(2)
