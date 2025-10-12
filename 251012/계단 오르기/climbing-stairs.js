ff8a25ca54ac00a44e71916c7bfa9443e04e6b9e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-12 22:12:30 +0900

    Title: 계단 오르기, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251012/계단 오르기/climbing-stairs.js
+++ 251012/계단 오르기/climbing-stairs.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
-const n = Number(fs.readFileSync(0, "utf8").trim());
+let input = fs.readFileSync(0).toString().trim()
 
+let n = Number(input)
 let dp = Array(n + 1).fill(0);
 
 dp[0] = 1;
 dp[1] = 0;
-if (n >= 2) dp[2] = 1;
-if (n >= 3) dp[3] = 1;
 
-for (let i = 4; i <= n; i++) {
-  dp[i] = dp[i - 2] + dp[i - 3];
+
+for (let i = 2; i <= n; i++) { 
+    if(i === 2 || i === 3) dp[i] = 1
+    else dp[i] = dp[i - 2] + dp[i - 3];
 }
 
-console.log(dp[n] % 100007);
+console.log(dp[n] % 10007);
