bffc3f7c019cd8fe92166f8164678f6edf6acbd0
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-12 22:45:28 +0900

    Title: 사각형 채우기 2, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251012/사각형 채우기 2/rectangle-fill-2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+
+
+let dp = Array(n+1).fill(0)
+const MOD = 10007
+dp[1] = 1
+dp[2] = 3
+
+for(let i = 3; i <=n; i++) {
+   dp[i] = (dp[i-1] + (dp[i-2] * 2)) % MOD
+}
+
+console.log(dp[n])
