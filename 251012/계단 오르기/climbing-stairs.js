71fbf74096de03481c866dc0a1f384d45b2a6edb
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-12 22:04:44 +0900

    Title: 계단 오르기, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251012/계단 오르기/climbing-stairs.js
+++ 251012/계단 오르기/climbing-stairs.js
@@ -6,7 +6,7 @@ const n = Number(input[0]);
 // Please Write your code here.
 
 let dp = Array(n+1).fill(0)
-
+dp[0] = 1
 for(let i = 2; i <= n; i++) {
     if(i === 2 || i === 3) dp[i] = 1
     else dp[i] = dp[i-2] + dp[i-3]
