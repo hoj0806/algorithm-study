e8260c2ab84c6bd5f80839988d2f6d94210b2486
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-13 20:57:04 +0900

    Title: 최대 점프 횟수, Time: 71ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251013/최대 점프 횟수/maximum-number-of-jumps.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const a = input[1].split(' ').map(Number);
+
+let dp = Array(n).fill(-1)
+dp[0] = 0
+
+for(let i = 1; i < n; i++) {
+
+  for(let j = 0; j < i; j++) {
+    if(a[j] + j >= i) dp[i] = Math.max(dp[i], dp[j] + 1)
+  }
+
+}
+
+
+console.log(Math.max(...dp))
