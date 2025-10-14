778f30f3b9f89e75dfd81dfdaf95b1b7d20691dd
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-15 04:37:51 +0900

    Title: 배낭 채우기, Time: 119ms, Memory: 26MB, Status: Passed - Codetree

---
+++ 251015/배낭 채우기/knapsack.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const jewels = input.slice(1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+
+// dp[i][j] = i번째까지의 보석까지 고려했을때 총 무게가 j을 넘지 않을때 최대 가치의 합
+
+
+let dp = Array(n+1).fill(0).map(() => Array(m+1).fill(0))
+
+for(let i = 1; i <= n; i++) {
+    for(let j = 1; j <= m; j++) {
+        let [weight, value] = jewels[i-1]
+        if(i === 1) {
+            if(weight <= j) dp[i][j] = value
+        } else {
+            if(j >= weight) {
+                dp[i][j] = Math.max(dp[i-1][j-weight] + value, dp[i-1][j]) 
+            } else {
+                dp[i][j] = dp[i-1][j]
+            }
+        }
+    }
+}
+
+console.log(dp[n][m])
