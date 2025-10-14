228db60ec8ec610a4276f130fd25dfb94da053da
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2025-10-15 03:57:24 +0900

    Title: 최대 동전 거슬러주기, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251015/최대 동전 거슬러주기/max-coin-change.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const coin = input[1].split(' ').map(Number);
+
+// Please write your code here.
+
+let dp = Array(m+1).fill(0)
+dp[0] = 0
+
+
+
+for(let i = 1; i <= n; i++) {
+    let max = -1
+    for(let j = 0; j < coin.length; j++) {
+        let c = coin[j]
+      
+        if(c <= i) max = Math.max(max, dp[c-i])
+    }
+
+    dp[i] = max
+}
+
+console.log(dp)
