2478e324d8d0ed84b8ee82d223e8c85b4464fbeb
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-01 08:12:00 +0900

    [Passed] 입력받은 값 교체하기 (69ms, 10MB)

--- trail0/입력받은 값 교체하기/changing-inputs.js
+++ trail0/입력받은 값 교체하기/changing-inputs.js
@@ -1,5 +1,12 @@
-const fs = require('fs')
-const p = fs.readFileSync(0).toString().trim().split(' ').map(Number)
+// 변수 선언, 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
 
+let a = Number(input[0]);
+let b = Number(input[1]);
 
-console.log(p[1], p[0])
+// 값 교체
+[a, b] = [b, a];
+
+// 출력
+console.log(a, b);
