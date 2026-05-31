a9cde8249661e67f3589de093bc40c2282fecffb
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-01 08:34:40 +0900

    [Passed] 시험 통과 여부 확인하기 (67ms, 10MB)

---
+++ trail0/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+let score = Number(fs.readFileSync(0).toString().trim())
+
+if(score >= 80) {
+    console.log("pass")
+} else {
+    console.log(`${80 - score} more score`)
+}
