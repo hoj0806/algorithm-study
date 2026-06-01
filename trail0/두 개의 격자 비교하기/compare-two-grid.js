ed7d1f2a8fa6b57f921649247f6001fd29e73643
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-02 08:49:40 +0900

    [Passed] 두 개의 격자 비교하기 (68ms, 12MB)

---
+++ trail0/두 개의 격자 비교하기/compare-two-grid.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim().split('\n')
+
+let [n, m] = f[0].split(" ").map(Number)
+let matrix1 = f.slice(1, n + 1).map(row => row.split(" ").map(Number))
+let matrix2 = f.slice(n + 1, f.length).map(row => row.split(" ").map(Number))
+let matrix3 = Array(n).fill(0).map(() => Array(m).fill(0))
+
+for(let i = 0; i < n; i++) {
+    for(let j = 0; j < m; j++) {
+        if(matrix1[i][j] === matrix2[i][j]) {
+            matrix3[i][j] = 0
+        } else {
+            matrix3[i][j] = 1
+        }
+    }
+    console.log(matrix3[i].join(" "))
+}
+
