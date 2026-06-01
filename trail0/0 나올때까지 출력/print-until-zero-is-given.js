33a20d813c0fc2f4840f8734443056204011b89b
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-02 04:06:36 +0900

    [Passed] 0 나올때까지 출력 (65ms, 10MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+let input = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+
+let idx = 0
+while(input[idx] !== 0) {
+    console.log(input[idx])
+    idx++
+}
+
