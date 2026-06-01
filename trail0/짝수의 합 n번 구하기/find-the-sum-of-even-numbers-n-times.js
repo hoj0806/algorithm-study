7b00268e2c495d29edab7e76c6117e400dc245e1
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-02 04:36:42 +0900

    [Passed] 짝수의 합 n번 구하기 (67ms, 10MB)

---
+++ trail0/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim().split('\n')
+
+let n = Number(f[0])
+
+for(let i = 1; i <= n; i++) {
+    let [a, b] = f[i].split(" ").map(Number)
+
+    let sum = 0
+    for(let j = a; j <= b; j++) {
+        if(j % 2 === 0) {
+            sum+=j
+        }
+    }
+
+    console.log(sum)
+}
