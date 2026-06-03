84b8f649a4ace79a30e729cd58f1a3ac32a1fdba
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-03 11:52:04 +0900

    [Passed] 비교에 따른 연산 (64ms, 10MB)

---
+++ trail1/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let [a, b] = f
+
+if(a > b) {
+    console.log(a * b)
+} else {
+    console.log(parseInt(b / a))
+}
