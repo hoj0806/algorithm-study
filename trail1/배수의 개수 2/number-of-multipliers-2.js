6511b514fbe3443f3f784d8cdaa2b92e6e53c1ca
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-03 16:31:06 +0900

    [Passed] 배수의 개수 2 (63ms, 10MB)

---
+++ trail1/배수의 개수 2/number-of-multipliers-2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+
+let cnt = 0
+
+for(let i = 0; i < f.length; i++) {
+    if(f[i] % 2 === 1) {
+        cnt++
+    }
+}
+
+console.log(cnt)
