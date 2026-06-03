43020d817fd76f36cf26c820b4afc7f15a7abef9
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-03 16:15:01 +0900

    [Passed] 높은 수에서 낮은 수까지 (63ms, 10MB)

---
+++ trail1/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let [a, b] = f
+
+let result = ''
+
+if(a >= b) {
+    for(let i = a; i >=b; i--) {
+        result += i + " "
+    }
+} else {
+     for(let i = b; i >=a; i--) {
+        result += i + " "
+    }
+}
+
+
+console.log(result)
