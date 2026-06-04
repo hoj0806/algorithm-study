e7f11002445f95a7e7e5104f9ac16c1acdfc73f7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-05 06:59:32 +0900

    [Passed] 학점 계산기 (61ms, 10MB)

--- trail1/학점 계산기/credit-calculator.js
+++ trail1/학점 계산기/credit-calculator.js
@@ -1,24 +1,23 @@
 const fs = require('fs')
 let f = fs.readFileSync(0).toString().trim().split('\n')
 
+let n = Number(f[0])
 let arr = f[1].split(" ").map(Number)
 
 
 let sum = 0
-let cnt = 0
 
 for (let ele of arr) {
     sum += ele
-    cnt++
 }
 
-let average = (sum / cnt).toFixed(1)
+let average = (sum / n).toFixed(1)
 
 
 console.log(average)
-if(average >= 4.0) {
+if (average >= 4.0) {
     console.log("Perfect")
-} else if(average >= 3.0) {
+} else if (average >= 3.0) {
     console.log("Good")
 } else {
     console.log("Poor")
