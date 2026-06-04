34dbeab5f16c0a0a675036d955d03633eddac376
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-05 04:51:59 +0900

    [Passed] ab 사이에 있는 c (60ms, 10MB)

---
+++ trail1/ab 사이에 있는 c/c-between-a-and-b.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+
+let [a, b, c] = f
+
+
+for (let i = a; i <= b; i++) {
+    if (i % c === 0) {
+        console.log("YES")
+        return
+    }
+}
+
+console.log("NO")
