5154649fdcd09745e47c6eaff2e75c9dd68d0db2
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-05 06:03:20 +0900

    [Passed] 별표 출력하기 7 (75ms, 10MB)

---
+++ trail1/별표 출력하기 7/print-star-7.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+let n = Number(f)
+
+for(let i = 1; i <= n; i++) {
+    let star = ''
+    for(let j = 1; j <= i; j++) {
+        star += "* "
+    }
+    console.log(star.trim())
+}
