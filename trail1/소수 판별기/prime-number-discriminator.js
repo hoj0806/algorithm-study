31419f45339c34eaa58cd969990fe973996987fa
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-05 05:03:44 +0900

    [Passed] 소수 판별기 (69ms, 10MB)

---
+++ trail1/소수 판별기/prime-number-discriminator.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+let n = Number(f)
+
+
+for(let i = 2; i < n-1; i++) {
+    if(n % i === 0) {
+        console.log("C")
+        return
+    }
+}
+
+console.log('P')
