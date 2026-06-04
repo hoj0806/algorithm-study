70687e1a321540153df673d5c45ae16cc2896aa5
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-05 06:24:13 +0900

    [Passed] 건너뛰고 별표 출력하기 (69ms, 11MB)

---
+++ trail1/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+
+let n = Number(f)
+let cnt = 1
+for (let i = 1; i <= (n * 2) - 1; i++) {
+    let star = ''
+    for (let j = 1; j <= cnt; j++) {
+        star += "*"
+    }
+
+    if(i < n) {
+        cnt++
+    } else {
+        cnt--
+    }
+
+    console.log(star)
+    console.log()
+}
