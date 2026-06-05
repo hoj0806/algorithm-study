e8ad32273cfe9c41da3ee605d4dc49cf461f14d5
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-06 04:26:11 +0900

    [Passed] 열 단위로 반복하여 출력 (63ms, 10MB)

---
+++ trail1/열 단위로 반복하여 출력/print-in-column.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+let n = Number(f)
+
+
+for (let i = 1; i <= n; i++) {
+    let str = ''
+
+    for (let j = 1; j <= n; j++) {
+        str += i
+    }
+
+    console.log(str)
+}
