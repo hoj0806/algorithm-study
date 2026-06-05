7bd3e59a8baa9ab26c120658841a614f26281849
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-06 07:10:03 +0900

    [Passed] 알파벳 출력하기 (65ms, 10MB)

---
+++ trail1/알파벳 출력하기/print-alphabet.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+let n = Number(f)
+
+let code = 65
+
+for (let i = 1; i <= n; i++) {
+    let str = ''
+
+    for (let j = 1; j <= i; j++) {
+        str += String.fromCharCode(code)
+        code++
+        if (code === 91) code = 65
+    }
+
+    console.log(str)
+}
