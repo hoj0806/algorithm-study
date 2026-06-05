8a4a7eeb42112f6ce7657bfb5aa4679f8a9de9af
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-06 06:14:52 +0900

    [Passed] 숫자 삼각형 (62ms, 10MB)

---
+++ trail1/숫자 삼각형/number-triangle.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+let n = Number(f)
+
+
+for (let i = 1; i <= n; i++) {
+    let str = ''
+
+    for (let j = 1; j <= i; j++) {
+        str += j + " "
+    }
+
+    console.log(str)
+
+}
