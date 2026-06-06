8ad37bfa370de80b8feb4b2027e8e3c8ab676370
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-06 12:27:05 +0900

    [Passed] 행에 따라 다르게 출력 (73ms, 12MB)

---
+++ trail1/행에 따라 다르게 출력/output-defferently-each-row.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+let f = fs.readFileSync(0).toString().trim()
+
+let n = Number(f)
+
+let cnt = 0
+
+
+for (let i = 1; i <= n; i++) {
+    let str = ''
+    for (let j = 1; j <= n; j++) {
+        if (i % 2 !== 0) {
+            cnt++
+        } else {
+            cnt += 2
+        }
+        str += cnt + " "
+
+    }
+    console.log(str)
+}
