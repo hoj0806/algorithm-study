5279e60d2f3e6fa90f2f19270907ca1c4d4416ce
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-08 12:00:35 +0900

    [Passed] 1부터 특정 수까지의 합 (67ms, 10MB)

---
+++ trail2/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+function solution(n) {
+    let sum = 0
+    for (let i = 1; i <= n; i++) {
+        sum += i
+    }
+
+    return parseInt(sum / 10)
+}
+
+console.log(solution(n))
