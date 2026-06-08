ab784bacf8c0879753e61c6633e19c61f3d57a32
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-08 15:08:47 +0900

    [Passed] 반복 출력하기 2 (72ms, 10MB)

---
+++ trail2/반복 출력하기 2/repeated-output-2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+
+function solution(n) {
+    if (n === 0) {
+        return
+    }
+
+    solution(n - 1)
+    console.log("HelloWorld")
+}
+
+solution(n)
