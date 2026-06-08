f8e5a5dc4a6933b38e564b4a97bb0026cb291975
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-08 17:07:54 +0900

    [Passed] k번째로 신기한 문자열 (65ms, 10MB)

---
+++ trail2/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let index = 0;
+const [n, k, t] = input[index++].split(' ');
+const words = [];
+for (let i = 0; i < Number(n); i++) {
+    words.push(input[index++]);
+}
+// Please Write your code here.
+
+
+let result = words.filter((i) => i.indexOf(t) === 0).sort()
+
+console.log(result[Number(k) - 1])
