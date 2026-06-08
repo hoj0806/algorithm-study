07b8706723304d8afcd63ef338b908aed36b8605
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-08 11:30:38 +0900

    [Passed] 반복 출력하기 (63ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+function print(n) {
+    for (let i = 1; i <= n; i++) {
+        process.stdout.write("12345^&*()_\n")
+    }
+}
+
+
+print(n)
