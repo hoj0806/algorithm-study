76286f0244165bb145db7483a4657c1353f6544e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-08 13:26:18 +0900

    [Passed] 짝수만 2로 나누기 (68ms, 10MB)

---
+++ trail2/짝수만 2로 나누기/divide-even-numbers-by-2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+// Please Write your code here.
+
+function solution(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            arr[i] /= 2
+        }
+    }
+}
+
+solution(arr)
+
+console.log(arr.join(" "))
