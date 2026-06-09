a968d5c91cd6c81a1845ac414ee2d15c2eb38212
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-09 16:49:47 +0900

    [Passed] 벌금은 누구에게 (67ms, 10MB)

---
+++ trail2/벌금은 누구에게/who-will-pay.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m, k] = input[0].split(" ").map(Number);
+const penalizedPersons = input.slice(1, m + 1).map(Number);
+
+// Please Write your code here.
+
+// n명의 학생 벌칙을 관리할 배열 만들기
+
+let arr = Array(n).fill(0)
+
+function check(arr, number) {
+    return arr.some((i) => i >= number)
+}
+
+// 순회하면서 체크
+let answer = -1
+
+for (let i = 0; i < penalizedPersons.length; i++) {
+    let p = penalizedPersons[i]
+    arr[p - 1] += 1
+    if (check(arr, k)) {
+        answer = p
+        break
+    }
+}
+
+console.log(answer)
