8acd569f5aa0965a88ea70f6b656f883887852cc
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-09 11:39:46 +0900

    [Passed] 여러가지 진수변환 (64ms, 10MB)

---
+++ trail2/여러가지 진수변환/various-numeral-system-transformations.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [n, b] = input[0].split(' ').map(Number);
+
+// Please Write your code here.
+
+let arr = []
+
+while (true) {
+    if (n < b) {
+        arr.push(n)
+        break
+    }
+
+    arr.push(n % b)
+    n = Math.floor(n / b)
+}
+
+console.log(arr.reverse().join(""))
