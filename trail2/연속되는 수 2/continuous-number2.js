e300e9747332ed7878c3075df194a65044494f51
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-09 16:11:25 +0900

    [Passed] 연속되는 수 2 (100ms, 43MB)

---
+++ trail2/연속되는 수 2/continuous-number2.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, n + 1).map(Number);
+
+// Please Write your code here.
+
+let cnt = 0
+let max = 0
+for (let i = 0; i < arr.length; i++) {
+    if (i === 0 || arr[i] !== arr[i - 1]) {
+        cnt = 1
+    } else {
+        cnt++
+    }
+
+    if (cnt > max) {
+        max = cnt
+    }
+}
+
+console.log(max)
