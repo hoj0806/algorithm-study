709221789292e93f38acd0a396c2820fb2ffb65a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-12 14:44:00 +0900

    [Passed] 최고의 13위치 (63ms, 10MB)

---
+++ trail2/최고의 13위치/best-place-of-13.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1).map(line => line.trim().split(' ').map(Number));
+
+// Please Write your code here.
+
+let max = 0
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - 2; j++) {
+        max = Math.max(max, arr[i][j] + arr[i][j + 1] + arr[i][j + 2])
+    }
+}
+
+console.log(max)
