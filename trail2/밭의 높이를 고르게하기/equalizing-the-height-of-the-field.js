acbc3ebbf2d229cdd88d3119750860856ab43e1e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-14 06:05:45 +0900

    [Passed] 밭의 높이를 고르게하기 (66ms, 10MB)

---
+++ trail2/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, h, t] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+
+let min = 0
+for (let i = 0; i < n - t + 1; i++) {
+    let sum = 0
+    for (let j = i; j < i + t; j++) {
+        sum += Math.abs(h - arr[j])
+    }
+    if(i === 0) min = sum
+    else min = Math.min(min, sum)
+}
+
+console.log(min)
