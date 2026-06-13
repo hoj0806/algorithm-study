765320622cd25b385f6a7778555ff00c7a865a1a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-14 07:25:54 +0900

    [Passed] 운행 되고 있는 시간 (80ms, 12MB)

---
+++ trail2/운행 되고 있는 시간/hours-in-service.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const segments = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+let max = -1
+
+for (let i = 0; i < n; i++) {
+    let arr = Array(1001).fill(0)
+    for (let j = 0; j < n; j++) {
+        if (i === j) {
+            continue
+        }
+
+        let [start, end] = segments[j]
+        for (let k = start; k < end; k++) {
+            arr[k] = 1
+        }
+    }
+    max = Math.max(max, arr.reduce((a, c) => a + c, 0))
+}
+
+console.log(max)
