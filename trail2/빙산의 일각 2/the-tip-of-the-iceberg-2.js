30307d36f5292ead3e9965a26768e51559804120
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-16 14:12:58 +0900

    [Passed] 빙산의 일각 2 (72ms, 10MB)

---
+++ trail2/빙산의 일각 2/the-tip-of-the-iceberg-2.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const h = input.slice(1, 1 + n).map(Number);
+
+// Please Write your code here.
+
+let maxHeight = Math.max(...h)
+
+let max = -1
+
+for (let i = 1; i <= maxHeight; i++) {
+    let over = false
+    let cnt = 0
+    for (let j = 0; j < n; j++) {
+
+        if (over === true) {
+            if (h[j] <= i) {
+                over = false
+            }
+        } else if (over === false) {
+            if (h[j] > i) {
+                over = true
+                cnt++
+            }
+        }
+        max = Math.max(max, cnt)
+    }
+}
+
+console.log(max)
