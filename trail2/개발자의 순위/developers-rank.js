556ee698cdb1dab162fdc499973c0ca3b44282f2
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-16 11:52:49 +0900

    [Passed] 개발자의 순위 (91ms, 22MB)

---
+++ trail2/개발자의 순위/developers-rank.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [k, n] = input[0].split(' ').map(Number);
+const arr = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+
+let list = []
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        if (i !== j) {
+            list.push(i + "," + j)
+        }
+    }
+}
+
+for (let i = 0; i < k; i++) {
+    let row = arr[i]
+    for (let j = 0; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            let s = row[k] + "," + row[j]
+            list = list.filter((i) => i !== s)
+        }
+    }
+}
+
+console.log(list.length)
