03445d09cb722bf834400e4b1553da431fe04917
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-19 03:37:01 +0900

    [Passed] 전부 겹치는 선분 (65ms, 10MB)

--- trail2/전부 겹치는 선분/overlapping-line-segments.js
+++ trail2/전부 겹치는 선분/overlapping-line-segments.js
@@ -2,23 +2,20 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = parseInt(input[0]);
-const segments = [];
+
+let max = 0
+let min = Infinity
+
 for (let i = 1; i <= n; i++) {
-    segments.push(input[i].split(' ').map(Number));
+    let [x1, x2] = input[i].split(" ").map(Number)
+    max = Math.max(max, x1)
+    min = Math.min(min, x2)
 }
 
-// Please Write your code here.
 
-for (let i = 0; i < n; i++) {
-    let [x1, y1] = segments[i]
-    for (let j = 0; j < n; j++) {
-        if (i === j) continue
-        let [x2, y2] = segments[j]
-        if (y1 < x2 || y2 < x1) {
-            console.log("No")
-            return
-        }
-    }
+if (min >= max) {
+    console.log("Yes")
+} else {
+    console.log("No")
 }
 
-console.log("Yes")
