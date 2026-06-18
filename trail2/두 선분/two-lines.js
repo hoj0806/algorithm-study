5edb5bbc7034bab1ec12a4c20e74ab4f0f4511f8
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-19 02:36:32 +0900

    [Passed] 두 선분 (64ms, 10MB)

---
+++ trail2/두 선분/two-lines.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [x1, x2, x3, x4] = input[0].split(' ').map(Number);
+// Please Write your code here.
+
+
+
+if (x2 < x3 || x4 < x1) {
+    console.log('nonintersecting')
+} else {
+    console.log('intersecting')
+}
