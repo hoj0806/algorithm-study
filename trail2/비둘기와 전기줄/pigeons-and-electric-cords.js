22243a370dd07de7b480c59529f1483a7dfb293e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-19 04:09:54 +0900

    [Passed] 비둘기와 전기줄 (68ms, 10MB)

---
+++ trail2/비둘기와 전기줄/pigeons-and-electric-cords.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const movements = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+let answer = 0
+for (let i = 1; i <= 10; i++) {
+    let cnt = 0
+    let left = []
+    let right = []
+    let p = ''
+    for (let j = 0; j < movements.length; j++) {
+        let [number, position] = movements[j]
+        if (i === number) {
+            cnt++
+            if (p === '') {
+                p = position
+            } else {
+                if (p !== position) {
+                    answer++
+                }
+                p = position
+            }
+        }
+    }
+}
+
+console.log(answer)
