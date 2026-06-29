5473abd214b3db9bc60f073c31fc5e6f8bc6804e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-06-30 06:52:05 +0900

    [Passed] 숫자가 더 큰 인접한 곳으로 이동 (64ms, 10MB)

---
+++ trail4/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [n, currX, currY] = input[0].split(' ').map(Number);
+let grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+
+let dx = [-1, 1, 0, 0]
+let dy = [0, 0, -1, 1]
+let x = currX - 1
+let y = currY - 1
+
+let answer = [grid[x][y]]
+
+function isRange(x, y) {
+  return x >= 0 && x < n && y >= 0 && y < n
+}
+
+while (true) {
+  let moved = false
+
+  for (let i = 0; i < dx.length; i++) {
+    let nx = x + dx[i]
+    let ny = y + dy[i]
+    if (isRange(nx, ny) && grid[nx][ny] > grid[x][y]) {
+      x = nx
+      y = ny
+      answer.push(grid[x][y])
+      moved = true
+      break
+    }
+  }
+
+
+
+
+
+  if (moved === false) {
+    break
+  }
+}
+
+console.log(answer.join(" "))
