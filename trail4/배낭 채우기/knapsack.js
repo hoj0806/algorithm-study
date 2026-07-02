151fed0ac4029094a0f7a8a102a04830ba98cb70
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-02 14:23:29 +0900

    [Passed] 배낭 채우기 (147ms, 26MB)

---
+++ trail4/배낭 채우기/knapsack.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const jewels = input.slice(1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+
+let dp = Array(n + 1).fill(-1).map(() => Array(m + 1).fill(-1))
+
+dp[0][0] = 0
+
+for (let i = 1; i <= n; i++) {
+    let [weight, value] = jewels[i - 1]
+    for (let j = 0; j <= m; j++) {
+        dp[i][j] = dp[i-1][j]
+
+        if(j >= weight && dp[i-1][j-weight] !== -1) {
+            dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight] + value)
+        }
+    }
+}
+
+let max = -1
+
+for(let i = 0; i < dp.length; i++) {
+    for(let j = 0; j < dp[i].length; j++) {
+        max = Math.max(max, dp[i][j])
+    }
+}
+
+console.log(max)
