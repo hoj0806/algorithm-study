85fd3ea7e4835330fa44868d185b614228b5685a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-02 12:43:29 +0900

    [Passed] 1, 2, 5 더하기 (70ms, 10MB)

---
+++ trail4/1, 2, 5 더하기/1-2-5-plus.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please write your code here.
+
+
+let dp = Array(n + 1).fill(0)
+let numbers = [1, 2, 5]
+
+dp[0] = 1
+
+for (let i = 1; i <= n; i++) {
+    let sum = 0
+
+    for (let j = 0; j < numbers.length; j++) {
+        if (i >= numbers[j]) {
+            if (dp[i - numbers[j]] === 0) continue
+
+            sum += (dp[i - numbers[j]]) 
+        }
+    }
+
+    dp[i] = sum % 10007
+}
+
+console.log(dp[n])
