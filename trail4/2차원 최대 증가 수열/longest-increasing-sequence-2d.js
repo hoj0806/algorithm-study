8e2f817b95c7d63437028f1a882f452b39ef5050
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-02 11:25:10 +0900

    [Passed] 2차원 최대 증가 수열 (73ms, 10MB)

--- trail4/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
+++ trail4/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
@@ -13,9 +13,7 @@ dp[0][0] = 1
 
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-        if (i === 0 || j === 0) continue
-
-
+    
         for (let x = 0; x < i; x++) {
             for (let y = 0; y < j; y++) {
                 if (grid[x][y] !== -1 && grid[i][j] > grid[x][y]) {
