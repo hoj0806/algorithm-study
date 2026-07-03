4d5d1b9634690a68d8240ba4c0ac14d318733b7f
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-03 15:28:13 +0900

    [Passed] 신전 탐험하기 (73ms, 10MB)

---
+++ trail4/신전 탐험하기/explore-temple.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const floors = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+
+
+let dp = Array(n).fill(0).map(() => Array(3).fill(0))
+
+
+for (let i = 0; i <= 2; i++) {
+    dp[0][i] = floors[0][i]
+}
+
+for (let j = 1; j < n; j++) {
+    for (let k = 0; k <= 2; k++) {
+        for (let p = 0; p <= 2; p++) {
+            if (k === p) continue
+            let newVal = dp[j-1][p] + floors[j][k]
+            dp[j][k] = Math.max(dp[j][k], newVal)
+        }
+    }
+}
+
+
+let max = -1
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= 2; j++) {
+        max = Math.max(max, dp[i][j])
+    }
+}
+
+
+console.log(max)
