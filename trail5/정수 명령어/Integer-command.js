b0db617cfd665bb820dede11f4b85d1f3090ae5e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-11 02:29:02 +0900

    [Passed] 정수 명령어 (483ms, 58MB)

---
+++ trail5/정수 명령어/Integer-command.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const SortedSet = require("collections/sorted-set");
+
+const t = Number(input[0]);
+let inputIndex = 1;
+
+for (let i = 0; i < t; i++) {
+    const k = Number(input[inputIndex]);
+    inputIndex++;
+    let s = new SortedSet()
+    for (let j = 0; j < k; j++) {
+        const [command, x] = input[inputIndex].split(" ");
+        const n = Number(x);
+        inputIndex++;
+        // Please Write your code here.
+        if(command === 'I') {
+            s.push(n)
+        } else if(command === 'D') {
+            if(n === 1) {
+                if(s.findGreatest()) {
+                    s.delete(s.findGreatest().value)
+                }
+            } else if(n === -1) {
+                if(s.findLeast()) {
+                    s.delete(s.findLeast().value)
+                }
+            }
+        }
+    }
+
+    if(s.findGreatest()) {
+        console.log(s.findGreatest().value, s.findLeast().value)
+    } else {
+        console.log("EMPTY")
+    }
+}
