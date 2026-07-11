af27a5c5eecd7b5ec8687c3e750e9e7bad1e6509
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-12 01:12:13 +0900

    [Passed] 배열 추출 (769ms, 47MB)

---
+++ trail5/배열 추출/array-extraction.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const Heap = require("collections/heap");
+
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n).map(Number);
+
+// Please Write your code here.
+
+const pq = new Heap();
+
+for (let i = 0; i < n; i++) {
+    let num = arr[i]
+    if (num === 0) {
+        if (pq.length === 0) {
+            console.log(0)
+        } else {
+            console.log(pq.pop())
+        }
+    } else {
+        pq.push(num)
+    }
+}
