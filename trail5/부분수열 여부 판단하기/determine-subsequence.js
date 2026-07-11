2b2842e408e88760575e4b761b9ad203c44e080a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-12 06:02:44 +0900

    [Passed] 부분수열 여부 판단하기 (87ms, 16MB)

---
+++ trail5/부분수열 여부 판단하기/determine-subsequence.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const A = input[1].split(' ').map(Number);
+const B = input[2].split(' ').map(Number);
+
+// Please Write your code here.
+
+
+function isSubsequence(target, source) {
+    let i = 0
+    let j = 0
+
+    while (j < source.length) {
+        if (target[i] === source[j]) {
+            i++
+        }
+
+        if (i === source.length) return true
+        j++
+    }
+
+    return i === target.length
+}
+
+
+
+console.log(isSubsequence(B, A) ? "Yes" : "No")
