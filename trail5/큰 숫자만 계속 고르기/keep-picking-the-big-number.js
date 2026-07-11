60fdcbbb2bd7e098794695b115a710609b9c5f60
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-12 00:14:46 +0900

    [Passed] 큰 숫자만 계속 고르기 (338ms, 25MB)

---
+++ trail5/큰 숫자만 계속 고르기/keep-picking-the-big-number.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const Heap = require("collections/heap");
+
+const [n, m] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+// Please Write your code here.
+
+const pq = new Heap();
+
+for (let i = 0; i < n; i++) {
+    pq.push(arr[i])
+}
+
+
+for (let i = 1; i <= m; i++) {
+    let top = pq.peek()
+    pq.delete(top)
+    pq.push(top-1)
+}
+
+console.log(pq.peek())
