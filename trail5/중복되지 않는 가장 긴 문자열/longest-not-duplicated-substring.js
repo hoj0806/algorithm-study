1a296800edc82e5007af63931da3b3babec83f80
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-13 03:13:40 +0900

    [Passed] 중복되지 않는 가장 긴 문자열 (90ms, 47MB)

---
+++ trail5/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const word = input[0].trim();
+const n = word.length;
+
+// Please Write your code here.
+
+
+let arr = Array(1000).fill(0)
+
+let j = 0
+let max = -1
+for (let i = 0; i < n; i++) {
+    while (j < n) {
+        let code = word[j].charCodeAt(0)
+        if (arr[code] + 1 >= 2) break
+        arr[code] += 1
+        j++
+    }
+    max = Math.max(max, j - i)
+    let code2 = word[i].charCodeAt(0)
+    arr[code2] -= 1
+}
+
+console.log(max)
