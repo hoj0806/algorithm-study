26d5efc9c6577b2c08b1a5ca81e74f18f1769aec
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-13 05:02:25 +0900

    [Passed] 서로 다른 구간의 수 (439ms, 53MB)

---
+++ trail5/서로 다른 구간의 수/number-of-distinct-segments.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = parseInt(input[0]);
+const arr = []
+for (let i = 1; i <= n; i++) {
+    const [a, b] = input[i].split(" ").map(Number);
+    arr.push([a, 1, i])
+    arr.push([b, -1, i])
+
+}
+
+let set = new Set()
+let sArr = arr.sort((a, b) => a[0] - b[0])
+let ans = 0
+
+for (let i = 0; i < sArr.length; i++) {
+    let [x, y, index] = sArr[i]
+
+    if (y === 1) {
+        if (set.size === 0) {
+            ans++
+        }
+        set.add(index)
+    } else if (y === -1) {
+        set.delete(index)
+    }
+}
+
+console.log(ans)
