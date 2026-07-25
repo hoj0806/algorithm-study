ec684d866f51b7ee0d0ebba843cc17052da82e99
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-25 17:52:06 +0900

    [Passed] 재귀함수를 이용한 피보나치 수 (88ms, 14MB)

--- trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
+++ trail2/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
@@ -4,12 +4,14 @@ const n = Number(input[0]);
 // Please Write your code here.
 
 
-function solution(n) {
+function fibo(n) {
     if (n === 1 || n === 2) {
         return 1
     }
 
-    return solution(n - 1) + solution(n - 2)
+    return fibo(n - 1) + fibo(n - 2)
 }
 
-console.log(solution(n))
+let answer = fibo(n)
+
+console.log(answer)
