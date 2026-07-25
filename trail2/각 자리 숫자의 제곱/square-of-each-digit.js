c6c0272a4913106f52d25e4bc4294da3665c59b8
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-25 17:43:01 +0900

    [Passed] 각 자리 숫자의 제곱 (66ms, 10MB)

--- trail2/각 자리 숫자의 제곱/square-of-each-digit.js
+++ trail2/각 자리 숫자의 제곱/square-of-each-digit.js
@@ -5,13 +5,15 @@ let n = Number(input[0]);
 
 // Please Write your code here.
 
+
 function solution(n) {
     if (n < 10) {
         return n ** 2
     }
 
-    return solution(parseInt(n / 10)) + (n % 10) ** 2
-
+    return solution(parseInt(n / 10)) + ((n % 10) ** 2)
 }
 
-console.log(solution(n))
+let answer = solution(n)
+
+console.log(answer)
