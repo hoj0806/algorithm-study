d50d60b44aa54cb765bc483e2260bf34461c183a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-25 17:31:12 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (70ms, 10MB)

--- trail2/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
+++ trail2/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let n = Number(input[0]);
 // Please Write your code here.
 
-function solution(n) {
+function print(n) {
     if (n === 0) {
         return
     }
@@ -11,12 +11,12 @@ function solution(n) {
     for (let i = 1; i <= n; i++) {
         process.stdout.write("* ")
     }
-    console.log()
-    solution(n - 1)
+    process.stdout.write("\n")
+    print(n - 1)
     for (let i = 1; i <= n; i++) {
         process.stdout.write("* ")
     }
-    console.log()
+    process.stdout.write("\n")
 }
 
-solution(n)
+print(n)
