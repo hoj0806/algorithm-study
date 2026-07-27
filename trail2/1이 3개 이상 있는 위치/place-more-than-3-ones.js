df5a251a8b63ac39db9dbd436b1d7858229a19a7
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-27 16:14:36 +0900

    [Passed] 1이 3개 이상 있는 위치 (72ms, 11MB)

--- trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.js
+++ trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.js
@@ -6,29 +6,31 @@ const grid = input.slice(1).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 
-let dx = [0, 1, 0, -1]
-let dy = [1, 0, -1, 0]
+
+function isRange(x, y) {
+  return x >= 0 && x < n && y >= 0 && y < n
+}
 
 let answer = 0
 
+let dx = [0, 1, 0, -1]
+let dy = [1, 0, -1, 0]
 
-function isRange(x, y) {
-    return x >= 0 && x < n && y >= 0 && y < n
-}
-
-for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid.length; j++) {
-        let cnt = 0
-
-        for (let k = 0; k < dx.length; k++) {
-            let x = i + dx[k]
-            let y = j + dy[k]
-            if (isRange(x, y) && grid[x][y] === 1) {
-                cnt++
-            }
-        }
-        if(cnt >= 3) answer++
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    let cnt = 0
+    let x = i
+    let y = j
+    for (let k = 0; k < dx.length; k++) {
+      let newX = x + dx[k]
+      let newY = y + dy[k]
+      if (isRange(newX, newY) && grid[newX][newY] === 1) {
+        cnt++
+      }
     }
+
+    if (cnt >= 3) answer++
+  }
 }
 
 console.log(answer)
