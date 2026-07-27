7717c5b0fd90e190bca383fdb83c27df9a83aef0
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-27 20:01:04 +0900

    [Passed] 병합 정렬 구현 (214ms, 40MB)

--- trail3/병합 정렬 구현/implement-merge-sort.js
+++ trail3/병합 정렬 구현/implement-merge-sort.js
@@ -6,24 +6,39 @@ const arr = input[1].trim().split(' ').map(Number);
 
 // Please Write your code here.
 
-function quick_sort(arr) {
-    if (arr.length <= 1) return arr
 
-    let pivot = arr[0]
-    let left = []
-    let right = []
+function merge(left, right) {
+    let result = []
+    let i = 0
+    let j = 0
+    let leftLen = left.length
+    let rightLen = right.length
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i])
+    while (i < leftLen && j < rightLen) {
+        if (left[i] > right[j]) {
+            result.push(right[j])
+            j++
         } else {
-            right.push(arr[i])
+            result.push(left[i])
+            i++
         }
     }
 
-    return [...quick_sort(left), pivot, ...quick_sort(right)]
+
+    return [...result, ...left.slice(i), ...right.slice(j)]
+}
+function merge_sort(arr) {
+    if (arr.length === 1) {
+        return arr
+    }
+
+
+    let len = arr.length
+    let mid = parseInt(len / 2)
+    let left = arr.slice(0, mid)
+    let right = arr.slice(mid)
+
+    return merge(merge_sort(left), merge_sort(right))
 }
 
-let answer = quick_sort(arr)
-
-console.log(answer.join(" "))
+console.log(merge_sort(arr).join(" "))
