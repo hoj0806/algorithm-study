c1f003258ae3ed9205a79fdfec7aacca13c6631e
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-27 18:38:48 +0900

    [Passed] 선택 정렬 구현 (65ms, 10MB)

--- trail3/선택 정렬 구현/implement-selection-sort.js
+++ trail3/선택 정렬 구현/implement-selection-sort.js
@@ -1,25 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
+
 const n = Number(input[0]);
 const arr = input[1].split(' ').map(Number);
 
 // Please Write your code here.
 
-function selection_sort(arr) {
-    const len = arr.length
 
-    for (let i = 0; i < len - 1; i++) {
-        let min_idx = i
-        for (let j = i + 1; j < len; j++) {
-            if (arr[min_idx] > arr[j]) {
-                min_idx = j
+function selection_sort(arr) {
+    let len = arr.length
+
+    for (let i = 0; i < len; i++) {
+        let min = Infinity
+        let minIndex = 0
+        for (let j = i; j < len; j++) {
+            if (min > arr[j]) {
+                min = Math.min(min, arr[j])
+                minIndex = j
             }
         }
-        let temp = arr[i]
-        arr[i] = arr[min_idx]
-        arr[min_idx] = temp
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
     }
-
 }
 
 selection_sort(arr)
