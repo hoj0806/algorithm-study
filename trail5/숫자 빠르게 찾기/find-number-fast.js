ecedcbf4c8c0a646583f0c0ff14d8d9e62caa7e6
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-28 18:24:29 +0900

    [Passed] 숫자 빠르게 찾기 (1130ms, 37MB)

--- trail5/숫자 빠르게 찾기/find-number-fast.js
+++ trail5/숫자 빠르게 찾기/find-number-fast.js
@@ -7,27 +7,28 @@ const queries = input.slice(2, 2 + m).map(Number);
 
 // Please Write your code here.
 
-
-function binarySearch(arr, target) {
+function binary_search(arr, target) {
     let left = 0
     let right = arr.length - 1
+
     while (left <= right) {
         let mid = parseInt((left + right) / 2)
+
         if (arr[mid] === target) {
-            // n번째를 찾아야하기 때문에 인덱스에 + 1
-            return mid +1
+            return mid + 1
+        }
+
+        if (arr[mid] > target) {
+            right = mid - 1
         } else {
-            if (arr[mid] > target) {
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
+            left = mid + 1
         }
     }
+
     return -1
 }
 
 for (let i = 0; i < m; i++) {
-    let q = queries[i]
-    console.log(binarySearch(numbers, q))
-} 
+    console.log(binary_search(numbers, queries[i]))
+}
+
