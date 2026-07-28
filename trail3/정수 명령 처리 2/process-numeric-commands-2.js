3712f0b4764a8e26291a9aa09f3df7658c5813b9
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-07-29 07:25:33 +0900

    [Passed] 정수 명령 처리 2 (272ms, 17MB)

--- trail3/정수 명령 처리 2/process-numeric-commands-2.js
+++ trail3/정수 명령 처리 2/process-numeric-commands-2.js
@@ -6,37 +6,33 @@ const commands = input.slice(1, n + 1);
 
 // Please write your code here.
 
+
 class Queue {
-    constructor(maxSize = 10000) {
+    constructor() {
         this.q = []
-        this.count = 0
-        this.maxSize = maxSize
-        this.head = 0
-        this.tail = 0
+        this.head = -1
+        this.tail = -1
     }
 
     push(item) {
-        this.tail = (this.tail + 1) % this.maxSize
-        this.q[this.tail] = item
-        this.count++
+        this.q.push(item)
+        this.tail++
     }
 
     pop() {
-        this.head = (this.head + 1) % this.maxSize
-        this.count--
-        return this.q[this.head]
+        return this.q[++this.head]
     }
 
     size() {
-        return this.count
+        return this.tail - this.head
     }
 
     empty() {
-        return this.count === 0 ? 1 : 0
+        return this.size() === 0 ? 1 : 0
     }
 
     front() {
-        return this.q[(this.head + 1) % this.maxSize]
+        return this.q[this.head + 1]
     }
 }
 
@@ -44,15 +40,16 @@ let q = new Queue()
 
 for (let i = 0; i < n; i++) {
     let [c, n] = commands[i].split(" ")
+    n = Number(n)
     if (c === "push") {
         q.push(n)
-    } else if (c === 'pop') {
-        console.log(q.pop())
-    } else if (c === 'size') {
-        console.log(q.size())
-    } else if (c === 'empty') {
-        console.log(q.empty())
-    } else if (c === 'front') {
+    } else if (c === "front") {
         console.log(q.front())
+    } else if (c === "size") {
+        console.log(q.size())
+    } else if (c === "empty") {
+        console.log(q.empty())
+    } else {
+        console.log(q.pop())
     }
 }
