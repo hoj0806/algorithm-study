43988f40791d30bf7573e3bc4ac77902a82dff1a
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-08-03 11:48:33 +0900

    [Passed] 최소 경로로 탈출 하기 (65ms, 10MB)

--- trail4/최소 경로로 탈출 하기/escape-with-min-distance.js
+++ trail4/최소 경로로 탈출 하기/escape-with-min-distance.js
@@ -6,23 +6,11 @@ const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
 
-
-const visited = Array(n).fill(0).map(() => Array(m).fill(false))
-const step = Array(n).fill(0).map(() => Array(m).fill(0))
-
-
-let dx = [0, 1, 0, -1]
-let dy = [1, 0, -1, 0]
-
-function isRange(x, y) {
-    return x >= 0 && x < n && y >= 0 && y < m
-}
-
 class Queue {
     constructor() {
         this.q = []
-        this.tail = -1
         this.head = -1
+        this.tail = -1
     }
 
     push(item) {
@@ -34,33 +22,46 @@ class Queue {
         return this.q[++this.head]
     }
 
+    size() {
+        return this.tail - this.head
+    }
+
     empty() {
-        return this.head === this.tail
+        return this.size() === 0
     }
 }
 
-const q = new Queue()
+let visited = Array(n).fill(false).map(() => Array(m).fill(false))
+let step = Array(n).fill(0).map(() => Array(m).fill(0))
 
-function bfs() {
+function isRange(x, y) {
+    return x >= 0 && x < n && y >= 0 && y < m
+}
+
+
+let q = new Queue()
+q.push([0, 0])
+
+let dx = [0, 1, 0, -1]
+let dy = [1, 0, -1, 0]
+
+function BFS() {
     while (!q.empty()) {
-        let curr = q.pop()
-        let [x, y] = curr
+        let currV = q.pop()
+        let [x, y] = currV
+        let s = step[x][y]
         for (let i = 0; i < dx.length; i++) {
-            let newX = x + dx[i]
-            let newY = y + dy[i]
-            if (isRange(newX, newY) && grid[newX][newY] === 1 && !visited[newX][newY]) {
-                visited[newX][newY] = true
-                step[newX][newY] = step[x][y] + 1
-                q.push([newX, newY])
+            let nx = x + dx[i]
+            let ny = y + dy[i]
+            if (isRange(nx, ny) && !visited[nx][ny] && grid[nx][ny] === 1) {
+                q.push([nx, ny])
+                step[nx][ny] = s + 1
+                visited[nx][ny] = true
             }
         }
     }
 }
 
-
-q.push([0, 0])
 visited[0][0] = true
-bfs()
-
-
+BFS()
 console.log(visited[n - 1][m - 1] ? step[n - 1][m - 1] : -1)
