9b85ad0aa990d06f2799a94b05ae5e5066c5252f
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2026-08-03 17:11:56 +0900

    [Passed] 마지막으로 남은 숫자 (134ms, 17MB)

--- trail5/마지막으로 남은 숫자/last-remaining-number.js
+++ trail5/마지막으로 남은 숫자/last-remaining-number.js
@@ -2,29 +2,97 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
-const Heap = require("collections/heap");
 
 // Please Write your code here.
 
-const pq = new Heap();
+class MaxPriorityQueue {
+    constructor() {
+        this.heap = []
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    isEmpty() {
+        return this.size() === 0
+    }
+
+    push(item) {
+        this.heap.push(item)
+        this._upheap()
+    }
+
+    pop() {
+        if (this.isEmpty()) return null;              // 1. 큐가 비어있으면 null 반환
+        if (this.size() === 1) return this.heap.pop(); // 2. 데이터가 1개뿐이면 그냥 pop()해서 반환 (길이가 0이 됨)
+
+        let top = this.heap[0];
+        this.heap[0] = this.heap.pop(); // 데이터가 2개 이상일 때만 마지막 값을 루트로 올림
+        this._downheap();
+        return top;
+    }
+
+    peek() {
+        return this.isEmpty() ? null : this.heap[0];
+    }
+
+    _upheap() {
+        let index = this.size() - 1
+        const current = this.heap[index]
+
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2)
+            let parent = this.heap[parentIndex]
+            if (parent >= current) break;
+
+            this.heap[index] = parent;
+            index = parentIndex;
+        }
+        this.heap[index] = current;
+    }
+
+    _downheap() {
+        let index = 0;
+        const size = this.size();
+        const current = this.heap[index];
+
+        while ((index * 2 + 1) < size) {
+            let leftChildIndex = index * 2 + 1;
+            let rightChildIndex = index * 2 + 2;
+            let largerIndex = leftChildIndex;
+
+
+            if (
+                rightChildIndex < size &&
+                this.heap[rightChildIndex] > this.heap[leftChildIndex]
+            ) {
+                largerIndex = rightChildIndex;
+            }
+
+            if (current >= this.heap[largerIndex]) break;
+
+            this.heap[index] = this.heap[largerIndex];
+            index = largerIndex;
+        }
+        this.heap[index] = current;
+    }
+}
+
+let q = new MaxPriorityQueue()
 
 for (let i = 0; i < n; i++) {
-    pq.push(arr[i])
+    q.push(arr[i])
 }
 
-while (true) {
-    if (pq.length < 2) {
-        if (pq.length === 0) {
-            console.log(-1)
-        } else {
-            console.log(pq.pop())
-        }
-        break
-    }
 
-    let top1 = pq.pop()
-    let top2 = pq.pop()
+while (q.size() > 1) {
+    let top1 = q.pop()
+    let top2 = q.pop()
+
     if (top1 !== top2) {
-        pq.push(top1 - top2)
+        q.push(top1 - top2)
     }
 }
+
+console.log(q.isEmpty() ? -1 : q.heap[0])
